b350a740cc8e97c817b9e5f7385f9453d850dd4e
CamilleBert <60038113+CamilleBert@users.noreply.github.com>
2022-04-15 15:39:58 +0200

    Update Menu.java

--- view/Menu.java
+++ view/Menu.java
@@ -1,61 +1,64 @@
 package view;
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 
-import java.awt.event.*;
-import java.awt.*;
-import java.util.*;
- 
-public class Menu extends JFrame{
-	// Define new buttons
-    JButton jb1 = new JButton("Gestion");
-    JButton jb2 = new JButton("Achat");
-     
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+
+public class Menu extends JFrame {
+
+	private JPanel p = new JPanel();;
+
 	
-    JFrame frame = new JFrame("Menu");
-    JPanel panel = new JPanel();
-    
-public Menu() {
 
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setLocationRelativeTo(null); 
-    frame.setResizable(false);
-    
-    panel.setBackground(Color.lightGray);
-    frame.add(panel, BorderLayout.CENTER);
-    panel.setBorder(new EmptyBorder(new Insets(250, 360, 250, 360)));
- 
-    
-    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-    
-    //bouton gestion
-    jb1.setMargin(new Insets(2,30,2,30));
-    jb1.setFont(new Font("Arial", Font.BOLD, 80));
-    jb1.setBackground(Color.GREEN);
-    panel.add(jb1);
-    panel.add(Box.createVerticalStrut(40));
-    
-    //bouton achat
-    jb2.setMargin(new Insets(2,70,2,70));
-    jb2.setFont(new Font("Arial", Font.BOLD, 80));
-    jb2.setBackground(Color.yellow);
-    panel.add(jb2);
-    panel.add(Box.createVerticalStrut(40));
-    
-    frame.add(panel);
-    frame.setSize(1200,800); 
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
+	JButton b_gestion = new JButton("GESTION");
+	JButton b_achats = new JButton("ACHATS");
+	public Menu() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1200, 800);
+		p.setBackground(new Color(248, 248, 255));
+		setContentPane(p);
+		p.setLayout(null);
+		setLocationRelativeTo(null);
+		
+	
+		
+		b_gestion.setBackground(new Color(147, 112, 219));
+		b_gestion.setFont(new Font("Tahoma", Font.BOLD, 55));
+		b_gestion.setBounds(360, 207, 386, 119);
+		b_gestion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		p.add(b_gestion);
+		
+		
+		b_achats.setFont(new Font("Tahoma", Font.BOLD, 55));
+		b_achats.setBackground(new Color(205, 92, 92));
+		b_achats.setBounds(360, 368, 386, 119);
+		b_achats.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		p.add(b_achats);
+	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu frame = new Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-	    }
-		  
-	  
- 
-    public static void main(String[] args) {
-        new Menu();
-    }
- 
-   
-    
 }
