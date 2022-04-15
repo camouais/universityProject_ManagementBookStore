830c1736c37646a7e61de266968cd1ad49b08d46
Spipo <68026807+spipanor@users.noreply.github.com>
2022-04-15 20:02:07 +0200

    Update Magasin.java

--- model/Magasin.java
+++ model/Magasin.java
@@ -3,28 +3,30 @@ package model;
 import java.util.*;
 
 public class Magasin {
-	//attributs
+	
+	// Attributs
+	
     public String m_nom;
     public String m_adresse;
     public Set<Employe> listEmp = new HashSet<>();
     public Set<Stock> listStock = new HashSet<>();
     public Set<Client> listClient = new HashSet<>();
-    public Set<Commande>  listCom = new HashSet<>();
-    public Set<Livre>  listLivre = new HashSet<>();
+    public Set<Commande> listCom = new HashSet<>();
+    public Set<Livre> listLivre = new HashSet<>();
     
+    // Constructeur
     
-    //Constructeur
     public Magasin(String name, String adr) {
     	m_nom = name;
     	m_adresse = adr;
     	
     }
     
-    //méthodes
+    // MÃ©thodes
     
-    ///////////////////////////////////////////
-    //---------------- COMMANDES---------------- 
-    ////////////////////////////////////////////
+    //////////////////////////////////////////////
+    //-----------------COMMANDES----------------//
+    //////////////////////////////////////////////
     
     // Enregistrement d'une commande 
     
@@ -32,7 +34,7 @@ public class Magasin {
     	listCom.add(c);
     }
 
-    //Recherche une commande par un id 
+    // Recherche d'une commande par son ID 
     
     public Commande rchCm1(int id) {
         Commande[] rch = new Commande[listCom.size()];
@@ -44,7 +46,8 @@ public class Magasin {
         return null;
     }
     
-  //lister les commandes qui ont la même date 
+    // Liste des commandes ayant la mÃªme date
+    
     public Set<Commande> rchCm(Date d) {
     	Set<Commande> sameDate = new HashSet<>();
         Commande[] t1 = new Commande[listCom.size()];
@@ -53,15 +56,16 @@ public class Magasin {
             	sameDate.add(listCom.toArray(t1)[i]);
             }
         }
-        return  sameDate;
+        return sameDate;
     }
-  //lister les commandes qui ont le même mode de paiement 
+    
+    // Liste des commandes ayant le mÃªme mode de paiement
+    
     public Set<Commande> rchCm(String mdp) {
     	Set<Commande> sameMdp = new HashSet<>();
         Commande[] t1 = new Commande[listCom.size()];
-        
         for (int i = 0; i < listCom.size(); i++) {
-            if( listCom.toArray(t1)[i].modePaiement == mdp) {
+            if(listCom.toArray(t1)[i].modePaiement == mdp) {
             	sameMdp.add(listCom.toArray(t1)[i]);
             }
         }
@@ -69,9 +73,11 @@ public class Magasin {
     }
     
     ///////////////////////////////////////////
-    //---------------- LIVRES---------------- 
-    ////////////////////////////////////////////
-    //enregistrer un livre 
+    //---------------- LIVRES----------------//
+    ///////////////////////////////////////////
+    
+    // Enregistrement d'un livre 
+    
     public int addLivre(Livre c) {
     	if(!listLivre.contains(c)) {
     		listLivre.add(c);
@@ -80,59 +86,17 @@ public class Magasin {
     	return -1;
     }
     
-   //suppprimer un livre 
-    public int remLivre(Livre c) {
-    	if(!listLivre.contains(c)) {
-    		listLivre.remove(c);
-    		return 0;
-    	}
-    	return -1;
+	////////////////////////////////////////////
+	//-----------------STOCKS-----------------//
+	////////////////////////////////////////////
+    
+    // CrÃ©ation d'un Stock
+    public void addStock(Livre c, int qE, int p) {
+    	Stock s = new Stock(p, qE, listStock.size()+1, c);
+    	listStock.add(s);
     }
-  //rechercher un livre 
-    public int Livre(Livre c) {
-    	if(!listLivre.contains(c)) {
-    		listLivre.remove(c);
-    		return 0;
-    	}
-    	return -1;
-    }
-	////////////////////////////////////////////
-	//--------------EXEMPLAIRES---------------//
-	////////////////////////////////////////////
-	
-	public int addEx(int id, int n) {
-		Stock[] t = new Stock[listStock.size()];
-		for (int j = 0; j < listStock.size(); j++) {
-			if(listStock.toArray(t)[j].idStock==id) {
-				for(int i = 0; i < n; i++) {
-					Exemplaire e = new Exemplaire(listStock.toArray(t)[j].exemplaires.size()+1, listStock.toArray(t)[j].livre);
-					listStock.toArray(t)[j].exemplaires.add(e);
-				}
-				return 0;
-			}
-		}
-		return -1;
-	}
-	
-	public int delEx(int id, int n) {
-		Stock[] t = new Stock[listStock.size()];
-		for (int j = 0; j < listStock.size(); j++) {
-			if(listStock.toArray(t)[j].idStock==id) {
-				for(int i = 0; i < n; i++) {
-					Exemplaire e = new Exemplaire(listStock.toArray(t)[j].exemplaires.size()+1, listStock.toArray(t)[j].livre);
-					listStock.toArray(t)[j].exemplaires.add(e);
-				}
-				return 0;
-			}
-		}
-		return -1;
-	}
-	///////////////////////////////////////////
-	//-----------------STOCKS----------------- 
-	////////////////////////////////////////////
-	
-	
-	//recherche un stock par un id
+    
+	// Recherche d'un stock par son ID
 	public Stock rchStock(int id) {
 		Stock[] t = new Stock[listStock.size()];
 		for (int i = 0; i < listStock.size(); i++) {
@@ -143,32 +107,34 @@ public class Magasin {
 		return null;
 	}
 	
-    ///////////////////////////////////////////
-    //-----------------CLIENTS----------------- 
-    ////////////////////////////////////////////
+    /////////////////////////////////////////////
+    //-----------------CLIENTS-----------------//
+    /////////////////////////////////////////////
 	
-	//recherche un client par nom et prénom
-    public Client rchCli(String n, String p) {
+    // Recherche d'un client par son nom et prÃ©nom
+	
+    public Client rchCli(String n, String p, int id) {
         Client[] t = new Client[listClient.size()];
         for (int i = 0; i < listCom.size(); i++) {
-            if((listClient.toArray(t)[i].nom==n) && (listClient.toArray(t)[i].prenom== p)) {
+            if((listClient.toArray(t)[i].nom==n) && (listClient.toArray(t)[i].prenom== p) && (listClient.toArray(t)[i].idClient == id)) {
                 return (Client) listClient.toArray()[i];
             }
         }
         return null;
     }
     
-    //ajouter un client
+    // Ajout d'un client
+    
     public int addCli(Client c) {
     	if(!listClient.contains(c)) {
     		listClient.add(c);
     		return 0;
     	} 
     	return -1;
-    	
     }
     
-    //supprimer un client
+    // Suppression d'un client
+    
     public int supCli(Client c) {
     	if(listClient.contains(c)) {
     		listClient.remove(c);
@@ -178,7 +144,8 @@ public class Magasin {
     	}
     }
     
-    //recherche un client par un id 
+    // Recherche d'un client par son ID
+    
     public Client rchClient(int id) {
     	Client[] rch = new Client[listClient.size()];
         for (int i = 0; i < listClient.size(); i++) {
@@ -189,7 +156,21 @@ public class Magasin {
         return null;
     }
     
-    //lister les commandes d'un client
+    // Recherche d'un client par son nom
+    
+    public Client listDatCompCli(String n) {
+    	Set<Client> sNom = new HashSet<>();
+    	Client[] tab = new Client[listClient.size()];
+        for (int i = 0; i < listClient.size(); i++) {
+        	if(listClient.toArray(tab)[i].nom == n) {
+        		sNom.add(listClient.toArray(tab)[i]);
+            }
+        }
+        return (Client) sNom;
+    }
+    
+    // Liste des commandes d'un client
+    
     public Commande listComCli(Client c) {
     	if(c.listCom!=null) {
     		return (Commande) c.listCom;
@@ -197,8 +178,9 @@ public class Magasin {
     	return null;
     }
     
-    //lister tous les clients qui ont la même date de création de compte
-    public Set<Client> listDatCompCli(Date d) {
+    // Liste des clients possÃ©dant la mÃªme date de crÃ©ation de compte
+    
+    public Client listDatCompCli(Date d) {
     	
     	Set<Client> s = new HashSet<>();
     	Client[] tab = new Client[listClient.size()];
@@ -207,38 +189,15 @@ public class Magasin {
             	s.add(listClient.toArray(tab)[i]);
             }
         }
-        return s;
+        return (Client) s;
     }
-    //lister tous les clients en fonction du nom
-    public Set<Client> listDatCompCli(String n) {
-    	
-    	Set<Client> sNom = new HashSet<>();
-    	Client[] tab = new Client[listClient.size()];
-        for (int i = 0; i < listClient.size(); i++) {
-        	if(listClient.toArray(tab)[i].nom == n) {
-        		sNom.add(listClient.toArray(tab)[i]);
-            }
-        }
-        return sNom;
-    }
-
-  //lister tous les clients qui ont la même adresse
-    public Set<Client> listAdCli(String ad) {
-    	
-    	Set<Client> sameAd = new HashSet<>();
-    	Client[] tab = new Client[listClient.size()];
-        for (int i = 0; i < listClient.size(); i++) {
-        	if(listClient.toArray(tab)[i].adresse == ad) {
-        		sameAd.add(listClient.toArray(tab)[i]);
-            }
-        }
-        return  sameAd;
-    }
-    ///////////////////////////////////////////
-    //-----------------MAGASIN----------------- 
-    ////////////////////////////////////////////
     
-    //modifier l'adresse du magasin
+    /////////////////////////////////////////////
+    //-----------------MAGASIN-----------------//
+    /////////////////////////////////////////////
+    
+    // Modification de l'adresse du magasin
+    
     public int updateAdr(String adr) {
 		if(adr != m_adresse) {
 			m_adresse=adr; 
@@ -249,31 +208,24 @@ public class Magasin {
 		}
     }
     
-    //modifier le nom du magasin
+    // Modification du nom du magasin
+    
     public int updateNom(String n) {
 		if(n != m_nom) {
 			m_nom=n; 
 			return 0;
 		}
 		else {
-			return -1;
+			return-1;
 		}
     }
     
-    ////////////////////////////////////////////
-    //-----------------EMPLOYES----------------- 
-    ////////////////////////////////////////////
-    //supprimer un employé
-    public int supEmp(Employe e) {
-    	if(listEmp.contains(e)) {
-    		listEmp.remove(e);
-    		return 0;
-    	} else {
-    		return -1;
-    	}
-    }
+    //////////////////////////////////////////////
+    //-----------------EMPLOYÃ‰S-----------------//
+    //////////////////////////////////////////////
     
-	//ajouter un employé
+    // Ajout d'un employÃ©
+    
     public int addEmp(Employe e) {
     	if(!listEmp.contains(e)) {
     		listEmp.add(e);
@@ -283,7 +235,31 @@ public class Magasin {
     	}
     }
     
-    // Recher un employé un employé par son nom et prénom
+    // Suppression d'un employÃ©
+    
+    public int supEmp(Employe e) {
+    	if(listEmp.contains(e)) {
+    		listEmp.remove(e);
+    		return 0;
+    	} else {
+    		return -1;
+    	}
+    }
+	
+    // Recherche d'un employÃ© par son ID
+    
+    public Employe rchEmp(int id) {
+    	Employe[] rch = new Employe[listEmp.size()];
+        for (int i = 0; i < listCom.size(); i++) {
+            if(listEmp.toArray(rch)[i].idEmp == id) {
+                return (Employe) listEmp.toArray()[i];
+            }
+        }
+        return null;
+    }
+    
+    // Recherche d'un employÃ© par son nom
+    
     public Set<Employe> listEmpNom(String n, String p) {
     	Set<Employe> sNom = new HashSet<>();
     	Employe[] tab = new Employe[listEmp.size()];
@@ -295,9 +271,9 @@ public class Magasin {
         return sNom;
     }
     
-    //lister tous les employés qui ont le même poste 
+    // Liste de tous les employÃ©s ayant le mÃªme poste
+    
     public Set<Employe> listEmpPos(String f) {
-    	
     	Set<Employe> samePoste = new HashSet<>();
     	Employe[] tab = new Employe[listEmp.size()];
         for (int i = 0; i < listEmp.size(); i++) {
@@ -308,9 +284,9 @@ public class Magasin {
         return samePoste;
     }
     
-    //lister tous les employés qui ont le même salaire 
+    // Liste de tous les employÃ©s ayant le mÃªme salaire
+    
     public Set<Employe> listEmpSalaire(float s) {
-    	
     	Set<Employe> sameSalai = new HashSet<>();
     	Employe[] tab = new Employe[listEmp.size()];
         for (int i = 0; i < listEmp.size(); i++) {
@@ -318,21 +294,9 @@ public class Magasin {
         		sameSalai.add(listEmp.toArray(tab)[i]);
             }
         }
-        return  sameSalai;
+        return sameSalai;
     }
-    //recherche un employe par un id 
-    public Employe rchEmp(int id) {
-    	Employe[] rch = new Employe[listEmp.size()];
-        for (int i = 0; i < listCom.size(); i++) {
-            if(listEmp.toArray(rch)[i].idEmp == id) {
-                return (Employe) listEmp.toArray()[i];
-            }
-        }
-        return null;
-    }
+    
     public static void main(String[] argv) {
-    	
-    	
-    	
     }
 }
