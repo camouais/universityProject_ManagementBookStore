57c0fadfef581bd30ef27a0e8924429741ab283d
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-17 00:41:38 +0200

    Update Fen_2_Gest.java

--- view/Fen_2_Gest.java
+++ view/Fen_2_Gest.java
@@ -1,15 +1,10 @@
-
 package view;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 import java.awt.Font;
-
 import javax.swing.JButton;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
@@ -17,65 +12,67 @@ import java.awt.Color;
 public class Fen_2_Gest extends JFrame {
 
 	private JPanel p = new JPanel();
-	JButton b_magasin = new JButton("MAGASIN");
-	JButton b_clients = new JButton("CLIENTS");
-	JButton b_livres = new JButton("LIVRES");
-	JButton b_retour = new JButton("RETOUR");
-	
-
 	
 	public Fen_2_Gest() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1200, 800);
-		setResizable(false);
-		setLocationRelativeTo(null);
-	    setVisible(true);
-		p.setBackground(new Color(153, 204, 255));
 		
-		setContentPane(p);
+		p.setBackground(new Color(200, 200, 200));
 		p.setLayout(null);
+		setContentPane(p);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 500, 650);
+		setLocationRelativeTo(null);
+		setResizable(false);
+	    setVisible(true);
 		
+		// Bouton "Livres"
 		
-		
-		//Bouton livres 
+		JButton b_livres = new JButton("LIVRES");
 		b_livres.setBackground(new Color(204, 204, 255));
 		b_livres.setFont(new Font("Tahoma", Font.BOLD, 55));
-		b_livres.setBounds(329, 51, 468, 119);
+		b_livres.setBounds(50, 50, 400, 100);
 		p.add(b_livres);
 		
-		//bouton magasin
+		// Bouton "Magasin"
 		
-		
+		JButton b_magasin = new JButton("MAGASIN");
 		b_magasin.setFont(new Font("Tahoma", Font.BOLD, 55));
 		b_magasin.setBackground(new Color(255, 255, 153));
-		b_magasin.setBounds(329, 375, 468, 119);
+		b_magasin.setBounds(50, 350, 400, 100);
+		b_magasin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Fen_3_Gest_InfoStat a = new Fen_3_Gest_InfoStat();
+				dispose();
+			}
+		});
 		p.add(b_magasin);
 		
-		//Bouton clients
+		// Bouton "Clients"
 		
+		JButton b_clients = new JButton("CLIENTS");
 		b_clients.setFont(new Font("Tahoma", Font.BOLD, 55));
 		b_clients.setBackground(new Color(102, 102, 255));
-		b_clients.setBounds(329, 207, 468, 119);
+		b_clients.setBounds(50, 200, 400, 100);
 		b_clients.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		p.add(b_clients);
 		
+		// Bouton "Retour"
 		
-		//Bouton retour
-		
-		b_retour.setBackground(new Color(255, 215, 0));
+		JButton b_retour = new JButton("RETOUR");
+		b_retour.setBackground(new Color(200, 100, 100));
 		b_retour.setFont(new Font("Tahoma", Font.BOLD, 30));
-		b_retour.setBounds(950, 572, 177, 63);
-		
+		b_retour.setBounds(50, 500, 200, 50);
 		b_retour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Fen_1 a = new Fen_1();
+				dispose();
 			}
 		});
 		p.add(b_retour);
-		
 	}
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -88,7 +85,6 @@ public class Fen_2_Gest extends JFrame {
 			}
 		});
 	}
-
 }
 
 
