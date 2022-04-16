93989427b525fe6763f0a3f103912afbcebfcfd8
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-17 00:41:14 +0200

    Update Fen_1.java

--- view/Fen_1.java
+++ view/Fen_1.java
@@ -1,6 +1,5 @@
 package view;
 
-
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -12,42 +11,53 @@ import java.awt.event.ActionEvent;
 import java.awt.Color;
 
 public class Fen_1 extends JFrame {
-
-	private JPanel p = new JPanel();;
-
-	JButton b_gestion = new JButton("GESTION");
-	JButton b_achats = new JButton("ACHATS");
 	
+	private JPanel p = new JPanel();
 	
 	public Fen_1() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1200, 800);
-		p.setBackground(new Color(248, 248, 255));
-		setContentPane(p);
-		p.setLayout(null);
-		setLocationRelativeTo(null);
 		
-	
+		// Fenêtre
+		
+		p.setBackground(new Color(200, 200, 200));
+		p.setLayout(null);
+		setContentPane(p);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 500, 400);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
+		
+		// Bouton "Gestion"
+		
+		JButton b_gestion = new JButton("GESTION");
 		
 		b_gestion.setBackground(new Color(147, 112, 219));
-		b_gestion.setFont(new Font("Tahoma", Font.BOLD, 55));
-		b_gestion.setBounds(360, 207, 386, 119);
+		b_gestion.setFont(new Font("Arial", Font.BOLD, 55));
+		b_gestion.setBounds(50, 50, 400, 100);
 		b_gestion.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Fen_2_Gest a = new Fen_2_Gest();
+				dispose();
 			}
 		});
 		p.add(b_gestion);
 		
+		// Bouton "Achats"
 		
-		b_achats.setFont(new Font("Tahoma", Font.BOLD, 55));
+		JButton b_achats = new JButton("ACHATS");
+		
+		b_achats.setFont(new Font("Arial", Font.BOLD, 55));
 		b_achats.setBackground(new Color(205, 92, 92));
-		b_achats.setBounds(360, 368, 386, 119);
+		b_achats.setBounds(50, 200, 400, 100);
 		b_achats.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Fen_2_Ach a = new Fen_2_Ach();
+				dispose();
 			}
 		});
 		p.add(b_achats);
 	}
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
