d93cfbb8c035aa238ab2967760fd0ce2d243ee91
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-17 00:42:08 +0200

    Update Fen_3_Gest_InfoStat.java

--- view/Fen_3_Gest_InfoStat.java
+++ view/Fen_3_Gest_InfoStat.java
@@ -25,18 +25,17 @@ public class Fen_3_Gest_InfoStat extends JFrame {
 
 	
 	public Fen_3_Gest_InfoStat() {
+		
+		p.setBackground(new Color(200, 200, 200));
+		p.setLayout(null);
+		setContentPane(p);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1200, 800);
-		setResizable(false);
 		setLocationRelativeTo(null);
-	    setVisible(true);
-		p.setBackground(new Color(153, 204, 255));
+		setResizable(false);
+		setVisible(true);
 		
-		setContentPane(p);
-		p.setLayout(null);
-		
-		
-		//Bouton Informations
+		// Bouton "Informations"
 		
 		b_info.setFont(new Font("Tahoma", Font.BOLD, 55));
 		b_info.setBackground(new Color(102, 102, 255));
@@ -47,7 +46,7 @@ public class Fen_3_Gest_InfoStat extends JFrame {
 		});
 		p.add(b_info);
 		
-		//Bouton statistiques
+		// Bouton "Statistiques"
 		
 		b_stat.setFont(new Font("Tahoma", Font.BOLD, 55));
 		b_stat.setBackground(new Color(0, 139, 139));
@@ -58,14 +57,15 @@ public class Fen_3_Gest_InfoStat extends JFrame {
 		});
 		p.add(b_stat);
 		
-		//Bouton retour
+		// Bouton "Retour"
 		
 		b_retour.setBackground(new Color(255, 215, 0));
 		b_retour.setFont(new Font("Tahoma", Font.BOLD, 30));
 		b_retour.setBounds(950, 572, 177, 63);
-		
 		b_retour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Fen_2_Gest a = new Fen_2_Gest();
+				dispose();
 			}
 		});
 		p.add(b_retour);
