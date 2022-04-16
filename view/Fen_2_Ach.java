6ed7c12a74eacd0396c79d38fb0de6a159498467
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-17 00:41:28 +0200

    Update Fen_2_Ach.java

--- view/Fen_2_Ach.java
+++ view/Fen_2_Ach.java
@@ -1,6 +1,4 @@
-
 package view;
-
 
 import java.awt.EventQueue;
 
@@ -12,61 +10,77 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 public class Fen_2_Ach extends JFrame {
 
 	private JPanel p = new JPanel();
 
 	public Fen_2_Ach() {
+		
+		// Fenêtre
+		
+		p.setBackground(new Color(200, 200, 200));
+		p.setLayout(null);
+		setContentPane(p);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1200, 800);
-		setResizable(false);
+		setBounds(100, 100, 500, 300);
 		setLocationRelativeTo(null);
+		setResizable(false);
 	    setVisible(true);
+	    
+		// JLabel Nouveau client ?
 		
-		setContentPane(p);//le container de la fenetre est p
-		p.setLayout(null); //null => oblige � ajouter la position de chaque composant
-		
-		
-		//JLabel Nouveau client ?
 		JLabel lblNewLabel = new JLabel("Nouveau client ?");
-		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 70));
-		lblNewLabel.setBounds(325, 234, 587, 145);
+		lblNewLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 60));
+		lblNewLabel.setBounds(0, 11, 484, 80);
 		p.add(lblNewLabel);
 		
-		//JButton Oui
+		// Bouton "Oui"
+		
 		JButton b_oui = new JButton("Oui");
 		b_oui.setBackground(new Color(0, 128, 0));
 		b_oui.setFont(new Font("Tahoma", Font.BOLD, 30));
-		b_oui.setBounds(192, 395, 150, 56);
+		b_oui.setBounds(50, 100, 150, 56);
 		b_oui.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Fen_3_Ach_NewC a = new Fen_3_Ach_NewC();
+				dispose();
 			}
 		});
 		p.add(b_oui);
 		
-		//JButton Non
+		// Bouton "Non"
+		
 		JButton b_non = new JButton("Non");
 		b_non.setBackground(new Color(255, 0, 0));
 		b_non.setFont(new Font("Tahoma", Font.BOLD, 30));
-		b_non.setBounds(783, 395, 150, 56);
+		b_non.setBounds(284, 100, 150, 56);
 		b_non.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Fen_3_Ach_ExistC a = new Fen_3_Ach_ExistC();
+				dispose();
 			}
 		});
 		p.add(b_non);
 		
-		//JButton Retour
+		// Bouton "Retour"
+		
 		JButton b_retour = new JButton("RETOUR");
-		b_retour.setFont(new Font("Tahoma", Font.BOLD, 30));
-		b_retour.setBackground(new Color(255, 215, 0));
-		b_retour.setBounds(950, 672, 177, 63);
+		b_retour.setFont(new Font("Tahoma", Font.BOLD, 20));
+		b_retour.setBackground(new Color(200, 100, 100));
+		b_retour.setBounds(165, 180, 150, 50);
 		b_retour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Fen_1 a = new Fen_1();
+				dispose();
 			}
 		});
 		p.add(b_retour);
 	}
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
