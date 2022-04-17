94bd57cff1d37ba4e6053365ea25cdb7491b98d8
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-17 14:45:56 +0200

    Update Fen4_Gest_MagInfo.java

--- view/Fen4_Gest_MagInfo.java
+++ view/Fen4_Gest_MagInfo.java
@@ -1,1 +1,97 @@
+package view;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Fen4_Gest_MagInfo extends JFrame {
+	private JPanel p = new JPanel();
+	private JTextField t_nom;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	public Fen4_Gest_MagInfo() {
+		
+		p.setBackground(new Color(200, 200, 200));
+		p.setLayout(null);
+		setContentPane(p);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 575, 320);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
+		
+		//JLabel Informations du Magasin
+		
+		JLabel l = new JLabel("Informations du magasin");
+		l.setHorizontalAlignment(SwingConstants.CENTER);
+		l.setFont(new Font("Tahoma", Font.BOLD, 40));
+		l.setBounds(0, 0, 550, 82); //définit la taille et la position (x, y, largeur, hauteur)
+		p.add(l);
+		
+		//JTextField nom
+		
+		t_nom = new JTextField();
+		t_nom.setBounds(150, 100, 380, 30);
+		t_nom.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		p.add(t_nom);
+		t_nom.setColumns(10);
+		
+		//JLabels nom, prénom, id
+		
+		JLabel l_nom = new JLabel("Nom :");
+		l_nom.setFont(new Font("Tahoma", Font.BOLD, 20));
+		l_nom.setBounds(20, 100, 150, 30);
+		p.add(l_nom);
+		
+		JLabel l_prenom = new JLabel("Adresse :");
+		l_prenom.setFont(new Font("Tahoma", Font.BOLD, 20));
+		l_prenom.setBounds(20, 140, 150, 30);
+		p.add(l_prenom);
+		
+		//Bouton Retour
+		
+		JButton b_retour = new JButton("RETOUR");
+		b_retour.setBackground(new Color(255, 215, 0));
+		b_retour.setFont(new Font("Tahoma", Font.BOLD, 20));
+		b_retour.setBounds(50, 200, 150, 50);
+		b_retour.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Fen3_Gest_Mag a = new Fen3_Gest_Mag();
+				dispose();
+			}
+		});
+		p.add(b_retour);
+		
+		//Bouton Enregistrer
+		
+		JButton b_valider = new JButton("ENREGISTRER");
+		b_valider.setBackground(new Color(0, 128, 0));
+		b_valider.setFont(new Font("Tahoma", Font.BOLD, 20));
+		b_valider.setBounds(300, 200, 200, 50);
+		b_valider.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		p.add(b_valider);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		textField.setColumns(10);
+		textField.setBounds(150, 141, 380, 30);
+		p.add(textField);
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Fen4_Gest_MagInfo frame = new Fen4_Gest_MagInfo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
