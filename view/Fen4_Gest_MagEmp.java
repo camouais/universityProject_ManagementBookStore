7723974c337f988a81f37629807e7f41d76f2720
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-17 14:46:49 +0200

    Update Fen4_Gest_MagEmp.java

--- view/Fen4_Gest_MagEmp.java
+++ view/Fen4_Gest_MagEmp.java
@@ -1,1 +1,151 @@
+package view;
 
+import model.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class Fen4_Gest_MagEmp extends JFrame {
+	
+	String[] data = {"sdfsd", "qsdfqsdf", "sdqsd", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "a", "dqsd", "sddq", "sddq", "sddq", "sddq", "sddq"
+			,"sdfsd", "qsdfqsdf", "sdqsd", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "a", "dqsd", "sddq", "sddq", "sddq", "sddq", "sddq"
+			,"sdfsd", "qsdfqsdf", "sdqsd", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "a", "dqsd", "sddq", "sddq", "sddq", "sddq", "sddq"
+			,"sdfsd", "qsdfqsdf", "sdqsd", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "a", "dqsd", "sddq", "sddq", "sddq", "sddq", "sddq"
+			,"sdfsd", "qsdfqsdf", "sdqsd", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "sddq", "a", "dqsd", "sddq", "sddq", "sddq", "sddq", "sddq"
+			};
+    
+    JList<String> list = new JList<String>();
+    JList<String> list2 = new JList<String>();
+    DefaultListModel<String> model;
+    JScrollPane scrollPane = new JScrollPane();
+    JScrollPane scrollPane2 = new JScrollPane();
+    public static int count = 0;
+	private JPanel p = new JPanel();
+
+	public Fen4_Gest_MagEmp() {
+		
+		// Fenêtre
+		
+		p = new JPanel();
+		p.setBorder(new EmptyBorder(5, 5, 5, 5));
+		p.setBackground(new Color(233, 150, 122));
+		p.setLayout(null);
+		setContentPane(p);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1000, 800);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
+		
+		// Panel 1 (Liste d'employés)
+		
+		JPanel panel1 = new JPanel();
+        list.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        scrollPane.setViewportView(list);
+        list.setLayoutOrientation(JList.VERTICAL);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel1.setBounds(50, 175, 400, 440);
+        panel1.setLayout(null);
+        scrollPane.setBounds(0, 0, 400, 440);
+        panel1.add(scrollPane);
+        p.add(panel1);
+        
+        // Panel 2 (Informations de l'employé sélectionné)
+        
+        model=new DefaultListModel<String>();
+        list2.setModel(model);
+        
+		JPanel panel2 = new JPanel();
+		list2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        scrollPane2.setViewportView(list2);
+        list2.setLayoutOrientation(JList.VERTICAL);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel2.setBounds(540, 175, 400, 345);
+		panel2.setLayout(null);
+		scrollPane2.setBounds(0, 0, 400, 345);
+		panel2.add(scrollPane2);
+		p.add(panel2);
+
+		// Textfield - Recherche d'un employé
+		
+		JTextField txtRecherchezUnLivre = new JTextField();
+		txtRecherchezUnLivre.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		txtRecherchezUnLivre.setText("Recherchez un employé...");
+		txtRecherchezUnLivre.setBounds(50, 100, 890, 45);
+		p.add(txtRecherchezUnLivre);
+		txtRecherchezUnLivre.setColumns(10);
+		
+		// JLabel "Employés"
+		
+		JLabel lblNewLabel = new JLabel("Employ\u00E9s");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 50));
+		lblNewLabel.setBounds(10, 15, 964, 57);
+		p.add(lblNewLabel);
+		
+		// Bouton "Tout Effacer"
+		
+		JButton btnModifier = new JButton("Modifier");
+		btnModifier.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnModifier.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnModifier.setBounds(540, 555, 400, 60);
+		btnModifier.setBackground(new Color(200, 200, 100));
+		btnModifier.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Fen5_Gest_ModifEmp a = new Fen5_Gest_ModifEmp();
+				dispose();
+			}
+		});
+		p.add(btnModifier);
+		
+		// Bouton "Ajouter"
+		
+		JButton btnAjouter = new JButton("Ajouter");
+		btnAjouter.setIcon(null);
+		btnAjouter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(list.getSelectedValue()!=null) {
+	                model.addElement("Test n°"+count);
+	                count++;
+                }           
+            }
+		});
+		btnAjouter.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnAjouter.setBounds(50, 650, 400, 60);
+		btnAjouter.setBackground(new Color(100, 200, 120));
+		p.add(btnAjouter);
+		
+		// Bouton "Enregistrer"
+        
+		JButton btnRetour = new JButton("RETOUR");
+		btnRetour.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Fen3_Gest_Mag a = new Fen3_Gest_Mag();
+				dispose();
+			}
+		});
+		btnRetour.setFont(new Font("Tahoma", Font.BOLD, 30));
+		btnRetour.setBounds(540, 650, 400, 60);
+		btnRetour.setBackground(new Color(200, 100, 100));
+		p.add(btnRetour);
+		
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Fen4_Gest_MagEmp frame = new Fen4_Gest_MagEmp();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
