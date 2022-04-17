30dce3c3be8a1fecdc565b7fdd4baf646f27e3d8
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-17 12:34:22 +0200

    Update Fen_5_Ach_Fact.java

--- view/Fen_5_Ach_Fact.java
+++ view/Fen_5_Ach_Fact.java
@@ -8,102 +8,105 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.SwingConstants;
 import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
 public class Fen_5_Ach_Fact extends JFrame {
-
-	private JPanel contentPane;
+	
 	private JTextField t_client;
 	private JTextField t_prix;
+	private JPanel p = new JPanel();
 	
 	public Fen_5_Ach_Fact() {
+		
+		p = new JPanel();
+		p.setBorder(new EmptyBorder(5, 5, 5, 5));
+		p.setBackground(new Color(150, 200, 150));
+		p.setLayout(null);
+		setContentPane(p);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 900, 755);
-		contentPane = new JPanel();
+		setBounds(0, 0, 500, 580);
 		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
 		
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		//panel
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(127, 255, 212));
-		panel.setBounds(182, 11, 445, 694);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
-		//JLabel Facture 
+		// JLabel Facture 
 		
 		JLabel l_facture = new JLabel("Facture ");
 		l_facture.setHorizontalAlignment(SwingConstants.CENTER);
-		l_facture.setFont(new Font("Tahoma", Font.PLAIN, 42));
-		l_facture.setBounds(109, 11, 227, 78);
-		panel.add(l_facture);
+		l_facture.setFont(new Font("Arial", Font.BOLD, 42));
+		l_facture.setBounds(0, 0, 484, 100);
+		p.add(l_facture);
 		
-		//JLabel Client
+		// JLabel "Client"
 		
 		JLabel l_client = new JLabel("Client : ");
-		l_client.setFont(new Font("Tahoma", Font.PLAIN, 27));
-		l_client.setBounds(32, 134, 105, 58);
-		panel.add(l_client);
+		l_client.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		l_client.setBounds(25, 100, 100, 30);
+		p.add(l_client);
 		
-		//Textfield client
+		// Textfield Client
 		
 		t_client = new JTextField();
 		t_client.setForeground(new Color(0, 0, 0));
 		t_client.setBackground(new Color(102, 255, 204));
-		t_client.setBounds(147, 150, 257, 30);
-		panel.add(t_client);
+		t_client.setBounds(125, 100, 325, 30);
+		p.add(t_client);
 		t_client.setColumns(10);
 		
-		
-		//JLabel Achats
+		// JLabel "Achats"
 		
 		JLabel l_achats = new JLabel("Achats : ");
-		l_achats.setFont(new Font("Tahoma", Font.PLAIN, 27));
-		l_achats.setBounds(32, 213, 105, 58);
-		panel.add(l_achats);
+		l_achats.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		l_achats.setBounds(25, 140, 100, 30);
+		p.add(l_achats);
 		
-		
-		//JPanel liste des achats 
+		// JPanel liste des achats 
 		
 		JPanel p_achats = new JPanel();
-		p_achats.setBounds(32, 282, 372, 236);
-		panel.add(p_achats);
+		p_achats.setBounds(25, 180, 432, 236);
+		p.add(p_achats);
 		
-		//JTextfield prix
+		// JTextfield prix
 		
 		t_prix = new JTextField();
-		t_prix.setBounds(147, 518, 254, 41);
-		panel.add(t_prix);
+		t_prix.setBounds(125, 425, 325, 30);
+		p.add(t_prix);
 		t_prix.setColumns(10);
-
 		
-		//JButton Enregistrer
+		// Bouton "Enregistrer"
+		
 		JButton b_enregistrer = new JButton("Enregistrer");
 		b_enregistrer.setBackground(new Color(255, 255, 0));
 		b_enregistrer.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		b_enregistrer.setBounds(244, 623, 160, 41);
-		panel.add(b_enregistrer);
+		b_enregistrer.setBounds(300, 475, 160, 41);
+		p.add(b_enregistrer);
 		
-		//JLabel Prix
+		// JLabel Prix
 		
 		JLabel l_prix = new JLabel("Prix : ");
-		l_prix.setFont(new Font("Tahoma", Font.PLAIN, 27));
-		l_prix.setBounds(32, 518, 105, 41);
-		panel.add(l_prix);
+		l_prix.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		l_prix.setBounds(25, 425, 100, 30);
+		p.add(l_prix);
 		
-		//JButton retour
+		// JButton retour
 		
 		JButton b_retour = new JButton("Retour");
 		b_retour.setBackground(new Color(255, 182, 193));
 		b_retour.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		b_retour.setBounds(32, 623, 160, 41);
-		panel.add(b_retour);
+		b_retour.setBounds(25, 475, 160, 41);
+		b_retour.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Fen_4_Ach_DoAch a = new Fen_4_Ach_DoAch();
+				dispose();
+			}
+		});
+		p.add(b_retour);
 	}
 	
 	public static void main(String[] args) {
