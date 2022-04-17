e62030d8ca9db3320d864ab71ea2832b4afe8372
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-17 12:46:43 +0200

    Update Fen_5_Ach_Fact.java

--- view/Fen_5_Ach_Fact.java
+++ view/Fen_5_Ach_Fact.java
@@ -1,20 +1,9 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.SwingConstants;
-import java.awt.Color;
-import javax.swing.JTextField;
-import javax.swing.JButton;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
 public class Fen_5_Ach_Fact extends JFrame {
 	
