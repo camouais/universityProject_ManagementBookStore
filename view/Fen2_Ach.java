4780b64309c3d75c55541f32ed7c30448814499c
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-18 20:49:12 +0200

    Update Fen2_Ach.java

--- view/Fen2_Ach.java
+++ view/Fen2_Ach.java
@@ -5,8 +5,15 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Fen2_Ach extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
 
 	private JPanel p = new JPanel();
+	private JLabel l_main = new JLabel("Nouveau client ?");
+	
+	JButton b_oui = new JButton("Oui");
+	JButton b_non = new JButton("Non");
+	JButton b_retour = new JButton("RETOUR");
 
 	public Fen2_Ach() {
 		
@@ -21,52 +28,44 @@ public class Fen2_Ach extends JFrame {
 		setResizable(false);
 	    setVisible(true);
 	    
-		// JLabel Nouveau client ?
+		// JLabel "Nouveau client ?"
 		
-		JLabel lblNewLabel = new JLabel("Nouveau client ?");
-		lblNewLabel.setVerticalAlignment(SwingConstants.BOTTOM);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 60));
-		lblNewLabel.setBounds(0, 11, 484, 80);
-		p.add(lblNewLabel);
+		l_main.setVerticalAlignment(SwingConstants.BOTTOM);
+		l_main.setHorizontalAlignment(SwingConstants.CENTER);
+		l_main.setFont(new Font("Calibri", Font.PLAIN, 60));
+		l_main.setBounds(0, 11, 484, 80);
+		p.add(l_main);
 		
-		// Bouton "Oui"
+		// Boutons : Oui, Non, Retour
 		
-		JButton b_oui = new JButton("Oui");
 		b_oui.setBackground(new Color(0, 128, 0));
 		b_oui.setFont(new Font("Tahoma", Font.BOLD, 30));
 		b_oui.setBounds(50, 100, 150, 56);
 		b_oui.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Fen3_Ach_NewC a = new Fen3_Ach_NewC();
+				new Fen3_Ach_NewC();
 				dispose();
 			}
 		});
 		p.add(b_oui);
 		
-		// Bouton "Non"
-		
-		JButton b_non = new JButton("Non");
 		b_non.setBackground(new Color(255, 0, 0));
 		b_non.setFont(new Font("Tahoma", Font.BOLD, 30));
 		b_non.setBounds(284, 100, 150, 56);
 		b_non.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Fen3_Ach_ExistC a = new Fen3_Ach_ExistC();
+				new Fen3_Ach_ExistC();
 				dispose();
 			}
 		});
 		p.add(b_non);
 		
-		// Bouton "Retour"
-		
-		JButton b_retour = new JButton("RETOUR");
 		b_retour.setFont(new Font("Tahoma", Font.BOLD, 20));
 		b_retour.setBackground(new Color(200, 100, 100));
 		b_retour.setBounds(165, 180, 150, 50);
 		b_retour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Fen1 a = new Fen1();
+				new Fen1();
 				dispose();
 			}
 		});
