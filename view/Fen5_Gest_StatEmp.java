bcfb63b036b36dea477f91f74144ad4437c12326
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-18 18:44:14 +0200

    Update Fen5_Gest_StatEmp.java

--- view/Fen5_Gest_StatEmp.java
+++ view/Fen5_Gest_StatEmp.java
@@ -1,30 +1,11 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTable;
-import javax.swing.JLabel;
-import javax.swing.JList;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import java.awt.Color;
-import java.awt.Button;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Vector;
-import java.awt.event.ActionEvent;
-import javax.swing.JButton;
-
 import javax.swing.table.*;
+
 public class Fen5_Gest_StatEmp extends JFrame {
 
     Object[][] donnees1 = {
@@ -53,19 +34,11 @@ public class Fen5_Gest_StatEmp extends JFrame {
             {"U", "7", "heiugeigeig", true, "ç",true, "&", "ss"},
             {"Y", "6", "heiugeigeig", false, "_",true, "&", "ss"},
             {"U", "7", "heiugeigeig", true, "ç",true, "&", "ss"}
-            
     };
-    Object[][] donnees2 = {
-            {"A", "1", "heiugeigeig"}
-            
-            
-    };
+    
     String[] entetes1 = {"Nom", "Prénom", "Age", "Sexe", "Salaire", "Fonction", "Ancienneté"};
-    String[] entetes2 = {"Total employé", "Total Salaire", "Age moyen"};
+    
     JScrollPane scrollPane = new JScrollPane();
-    
-    
-    
     
     JLabel l_stem = new JLabel("Statistiques au niveau des employés ");
     JLabel l_valueAg = new JLabel("ag");
@@ -76,6 +49,7 @@ public class Fen5_Gest_StatEmp extends JFrame {
     JLabel l_agm = new JLabel("Age moyen :");
     JLabel l_nbrt = new JLabel("Nombre total d'employés :");
     JPanel panel = new JPanel();
+    
 	public Fen5_Gest_StatEmp() {
 		
 		// Fenêtre 
@@ -87,23 +61,22 @@ public class Fen5_Gest_StatEmp extends JFrame {
 		setVisible(true);
 		setContentPane(panel);
 	    panel.setLayout(null);
-	    
+	    panel.setBackground(new Color(0, 222, 100));
 		
 		// Tableau
 	   
 	    JTable table1 = new JTable(donnees1, entetes1); 
 	    table1.setAutoCreateRowSorter(true);
-	    table1.setBounds(100,150,800,400);
+	    table1.setBounds(100, 150, 800, 400);
 	    JScrollPane scrollPane_1 = new JScrollPane(table1);
-	    scrollPane_1.setBounds(100,150,800,400);
+	    scrollPane_1.setBounds(100, 150, 800, 400);
 	    panel.add(scrollPane_1);
 	    
-	    // JLabel Statistiques au niveau des employés
-	    
+	    // JLabel "Statistiques au niveau des employés"
 	    
 	    l_stem.setHorizontalAlignment(SwingConstants.CENTER);
-	    l_stem.setFont(new Font("Tahoma", Font.PLAIN, 40));
-	    l_stem.setBounds(144, 78, 704, 42);
+	    l_stem.setFont(new Font("Tahoma", Font.BOLD, 40));
+	    l_stem.setBounds(125, 50, 750, 42);
 	    panel.add(l_stem);
 
 	    l_nbrt.setFont(new Font("Tahoma", Font.PLAIN, 25));
@@ -122,7 +95,6 @@ public class Fen5_Gest_StatEmp extends JFrame {
 	    l_valueT.setBounds(799, 591, 47, 42);
 	    panel.add(l_valueT);
 	    
-	    
 	    l_valueAg.setFont(new Font("Tahoma", Font.PLAIN, 25));
 	    l_valueAg.setBounds(799, 644, 47, 42);
 	    panel.add(l_valueAg);
@@ -130,7 +102,6 @@ public class Fen5_Gest_StatEmp extends JFrame {
 	    l_valueS.setFont(new Font("Tahoma", Font.PLAIN, 25));
 	    l_valueS.setBounds(799, 696, 64, 42);
 	    panel.add(l_valueS);
-	    
 	   
 	    b_retour.setFont(new Font("Tahoma", Font.PLAIN, 33));
 	    b_retour.addActionListener(new ActionListener() {
@@ -143,11 +114,9 @@ public class Fen5_Gest_StatEmp extends JFrame {
 	    b_retour.setBackground(new Color(64,128,0));
 	    
 	    panel.add(b_retour);
-
-	  
-	    
-	 
-	}public static void main(String[] args) {
+	}
+	
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
