eb1fadae1ff4d9fa2300c63abf1af5d88623ab24
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-18 20:49:20 +0200

    Update Fen2_Gest.java

--- view/Fen2_Gest.java
+++ view/Fen2_Gest.java
@@ -5,10 +5,19 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Fen2_Gest extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
 
 	private JPanel p = new JPanel();
 	
+	JButton b_livres = new JButton("LIVRES");
+	JButton b_magasin = new JButton("MAGASIN");
+	JButton b_clients = new JButton("CLIENTS");
+	JButton b_retour = new JButton("RETOUR");
+	
 	public Fen2_Gest() {
+		
+		// Fenêtre
 		
 		p.setBackground(new Color(200, 200, 200));
 		p.setLayout(null);
@@ -19,49 +28,41 @@ public class Fen2_Gest extends JFrame {
 		setResizable(false);
 	    setVisible(true);
 		
-		// Bouton "Livres"
+		// Boutons : Livres, Clients, Magasin, Retour
 		
-		JButton b_livres = new JButton("LIVRES");
 		b_livres.setBackground(new Color(204, 204, 255));
 		b_livres.setFont(new Font("Tahoma", Font.BOLD, 55));
 		b_livres.setBounds(50, 50, 400, 100);
 		p.add(b_livres);
 		
-		// Bouton "Magasin"
-		
-		JButton b_magasin = new JButton("MAGASIN");
 		b_magasin.setFont(new Font("Tahoma", Font.BOLD, 55));
 		b_magasin.setBackground(new Color(255, 255, 153));
 		b_magasin.setBounds(50, 350, 400, 100);
 		b_magasin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Fen3_Gest_Mag a = new Fen3_Gest_Mag();
+				new Fen3_Gest_Mag();
 				dispose();
 			}
 		});
 		p.add(b_magasin);
 		
-		// Bouton "Clients"
-		
-		JButton b_clients = new JButton("CLIENTS");
 		b_clients.setFont(new Font("Tahoma", Font.BOLD, 55));
 		b_clients.setBackground(new Color(102, 102, 255));
 		b_clients.setBounds(50, 200, 400, 100);
 		b_clients.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				new Fen3_Gest_Cli();
+				dispose();
 			}
 		});
 		p.add(b_clients);
 		
-		// Bouton "Retour"
-		
-		JButton b_retour = new JButton("RETOUR");
 		b_retour.setBackground(new Color(200, 100, 100));
 		b_retour.setFont(new Font("Tahoma", Font.BOLD, 30));
 		b_retour.setBounds(50, 500, 200, 50);
 		b_retour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Fen1 a = new Fen1();
+				new Fen1();
 				dispose();
 			}
 		});
