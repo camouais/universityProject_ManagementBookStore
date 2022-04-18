465a3f6a202ba134e313d92c3a097e37667cee9d
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-18 20:53:04 +0200

    Create Fen5_Gest_StatComp.java

---
+++ view/Fen5_Gest_StatComp.java
@@ -0,0 +1,91 @@
+package view;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Fen5_Gest_StatComp extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel p = new JPanel();
+    private JLabel l_stem = new JLabel("Comptabilités");
+    private JLabel l_valueAg = new JLabel("tv");
+    private JLabel l_valueT = new JLabel("nb");
+    private JLabel l_nbrt = new JLabel("Nombre total de livres : ");
+    private JLabel l_soms = new JLabel("Total de ventes : ");
+    
+    JScrollPane scrollPane = new JScrollPane();
+    JButton b_retour = new JButton("Retour");
+    
+	public Fen5_Gest_StatComp() {
+		
+		// Fenêtre
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1000, 800);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
+		setContentPane(p);
+	    p.setLayout(null);
+		p.setBackground(new Color(222, 206, 250));
+	    
+	    // JLabel "Statistiques au niveau des livres"
+	    
+	    l_stem.setHorizontalAlignment(SwingConstants.CENTER);
+	    l_stem.setFont(new Font("Tahoma", Font.BOLD, 40));
+	    l_stem.setBounds(125, 50, 750, 42);
+	    p.add(l_stem);
+	    
+	    // JLabel "Nombre total de livres"
+
+	    l_nbrt.setFont(new Font("Tahoma", Font.PLAIN, 25));
+	    l_nbrt.setBounds(472, 591, 341, 42);
+	    p.add(l_nbrt);
+	    
+	    // JLabel "Total de ventes"
+
+	    l_soms.setFont(new Font("Tahoma", Font.PLAIN, 25));
+	    l_soms.setBounds(472, 643, 341, 42);
+	    p.add(l_soms);
+	    
+	    // JLabel : Valeur totale de livres
+	    
+	    l_valueT.setFont(new Font("Tahoma", Font.PLAIN, 25));
+	    l_valueT.setBounds(799, 591, 47, 42);
+	    p.add(l_valueT);
+	    
+	    // JLabel : Valeur totale de ventes
+	    
+	    l_valueAg.setFont(new Font("Tahoma", Font.PLAIN, 25));
+	    l_valueAg.setBounds(799, 644, 47, 42);
+	    p.add(l_valueAg);
+	    
+	    // Bouton : Retour
+	   
+	    b_retour.setFont(new Font("Tahoma", Font.PLAIN, 33));
+	    b_retour.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new Fen4_Gest_MagStat();
+				dispose();
+			}
+		});
+	    b_retour.setBounds(71, 634, 222, 57);
+	    b_retour.setBackground(new Color(64,128,0));
+	    p.add(b_retour);
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Fen5_Gest_StatComp frame = new Fen5_Gest_StatComp();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
