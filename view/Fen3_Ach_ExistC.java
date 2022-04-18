7e5fd765475dc16aeb8a204662acee45b57fbcb8
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-18 20:49:28 +0200

    Update Fen3_Ach_ExistC.java

--- view/Fen3_Ach_ExistC.java
+++ view/Fen3_Ach_ExistC.java
@@ -5,11 +5,20 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Fen3_Ach_ExistC extends JFrame {
-
+	
+	private static final long serialVersionUID = 1L;
+	
 	private JPanel p = new JPanel();
 	private JTextField t_nom;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField t_prenom = new JTextField();
+	private JTextField t_id = new JTextField();
+	private JLabel l_main = new JLabel("Renseignements du client");
+	private JLabel l_nom = new JLabel("Nom :");
+	private JLabel l_prenom = new JLabel("Prénom :");
+	private JLabel l_id = new JLabel("Identifiant :");
+	
+	JButton b_valider = new JButton("VALIDER");
+	JButton b_retour = new JButton("RETOUR");
 
 	public Fen3_Ach_ExistC() {
 		
@@ -22,78 +31,66 @@ public class Fen3_Ach_ExistC extends JFrame {
 		setResizable(false);
 		setVisible(true);
 		
-		//JLabel Renseignements du client
+		// Labels : "Renseignements du client", "Nom", "Prénom", "Identifiant"
 		
-		JLabel l = new JLabel("Renseignements du client");
-		l.setHorizontalAlignment(SwingConstants.CENTER);
-		l.setFont(new Font("Tahoma", Font.BOLD, 40));
-		l.setBounds(0, 0, 550, 82); //définit la taille et la position (x, y, largeur, hauteur)
-		p.add(l);
+		l_main.setHorizontalAlignment(SwingConstants.CENTER);
+		l_main.setFont(new Font("Tahoma", Font.BOLD, 40));
+		l_main.setBounds(0, 0, 550, 82);
+		p.add(l_main);
 		
-		//JTextField nom
+		l_nom.setFont(new Font("Tahoma", Font.BOLD, 20));
+		l_nom.setBounds(20, 100, 150, 30);
+		p.add(l_nom);
+		
+		l_prenom.setFont(new Font("Tahoma", Font.BOLD, 20));
+		l_prenom.setBounds(20, 140, 150, 30);
+		p.add(l_prenom);
+		
+		l_id.setFont(new Font("Tahoma", Font.BOLD, 20));
+		l_id.setBounds(20, 180, 150, 30);
+		p.add(l_id);
+		
+		// Text Fields : Nom, Prénom, Identifiant
 		
 		t_nom = new JTextField();
 		t_nom.setBounds(150, 100, 380, 30);
 		t_nom.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		p.add(t_nom);
 		t_nom.setColumns(10);
+
+		t_prenom.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		t_prenom.setColumns(10);
+		t_prenom.setBounds(150, 141, 380, 30);
+		p.add(t_prenom);
 		
-		//JLabels nom, prénom, id
+		t_id.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		t_id.setColumns(10);
+		t_id.setBounds(150, 180, 380, 30);
+		p.add(t_id);
 		
-		JLabel l_nom = new JLabel("Nom :");
-		l_nom.setFont(new Font("Tahoma", Font.BOLD, 20));
-		l_nom.setBounds(20, 100, 150, 30);
-		p.add(l_nom);
+		// Boutons : Retour, Valider
 		
-		JLabel l_prenom = new JLabel("Prénom :");
-		l_prenom.setFont(new Font("Tahoma", Font.BOLD, 20));
-		l_prenom.setBounds(20, 140, 150, 30);
-		p.add(l_prenom);
-		
-		JLabel l_id = new JLabel("Identifiant :");
-		l_id.setFont(new Font("Tahoma", Font.BOLD, 20));
-		l_id.setBounds(20, 180, 150, 30);
-		p.add(l_id);
-		
-		//Bouton Retour
-		
-		JButton b_retour = new JButton("RETOUR");
 		b_retour.setBackground(new Color(255, 215, 0));
 		b_retour.setFont(new Font("Tahoma", Font.BOLD, 20));
 		b_retour.setBounds(50, 230, 150, 50);
 		b_retour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Fen2_Ach a = new Fen2_Ach();
+				new Fen2_Ach();
 				dispose();
 			}
 		});
 		p.add(b_retour);
 		
-		//Bouton Valider
-		
-		JButton b_valider = new JButton("VALIDER");
 		b_valider.setBackground(new Color(0, 128, 0));
 		b_valider.setFont(new Font("Tahoma", Font.BOLD, 20));
 		b_valider.setBounds(350, 230, 150, 50);
 		b_valider.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Fen4_Ach_DoAch a = new Fen4_Ach_DoAch();
+				new Fen4_Ach_DoAch();
 				dispose();
 			}
 		});
 		p.add(b_valider);
-		
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		textField.setColumns(10);
-		textField.setBounds(150, 141, 380, 30);
-		p.add(textField);
-		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		textField_1.setColumns(10);
-		textField_1.setBounds(150, 180, 380, 30);
-		p.add(textField_1);
 	}
 	
 	public static void main(String[] args) {
