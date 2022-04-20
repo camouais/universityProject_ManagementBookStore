b5d891e4a552ee863f1058f09790c5a6a3b9d4f8
CamilleBert <60038113+CamilleBert@users.noreply.github.com>
2022-04-20 20:15:36 +0200

    Create NewMagasin.java

---
+++ controller/NewMagasin.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import model.*;
+import view.*;
+
+public class NewMagasin implements ActionListener {
+	Magasin magasin;
+	JTextField nom;
+	JTextField adresse;
+	
+	public NewMagasin(JTextField n, JTextField a) {
+		this.nom=n;
+		this.adresse=a;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		magasin= new Magasin(nom.getText(), adresse.getText());
+		
+	}
+}
