e1f9acc9e4e5b561469fed9e84ba1c3c201ecd5c
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-20 21:28:45 +0200

    Update Commande.java

--- model/Commande.java
+++ model/Commande.java
@@ -1,33 +1,26 @@
 package model;
 
 import java.util.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 
- public class Commande  {
-	 
-	 // Attributs
-	 
-	 public static int totalCom;
-     public Date dateAchat;
-	 public String modePaiement;
-	 public Magasin mag;
-	 public int idCom;
-	 public Client client;
-	 public Set<Livre> listLivres;
-	 
-	 // Constructeur
-	 
-	 public Commande(Date dA, Client c, String mdp, int id) {
-		 dateAchat= dA;
-		 modePaiement = mdp;
-		 client=c;
-		 idCom =id;
-		 totalCom++;
-	 }
-	 
-	 // Méthodes
-	 
-	 
+public class Commande  {
+	
+	// Attributs
+	
+	public static int totalCom;
+	public Date dateAchat;
+	public String modePaiement;
+	public Magasin mag;
+	public int idCom;
+	public Client client;
+	public Set<Livre> listLivres;
+	
+	// Constructeur
+	
+	public Commande(Date dA, Client c, String mdp, int id) {
+		dateAchat= dA;
+		modePaiement = mdp;
+		client=c;
+		idCom =id;
+		totalCom++;
+	}
 }
