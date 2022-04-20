4b11087f9e7e4127c050d0051451ac7aa00939f7
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-20 21:29:12 +0200

    Update NewClient.java

--- controller/NewClient.java
+++ controller/NewClient.java
@@ -1,10 +1,14 @@
 package controller;
 
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 import model.*;
+import view.*;
 
-public class NewClient implements ActionListener{
+public class NewClient implements ActionListener {
+	
 	Client c;
 	JTextField nom;
 	JTextField prenom;
@@ -14,20 +18,16 @@ public class NewClient implements ActionListener{
 	JTextField mail;
 	JTextField sexe;
 	
-	public static int id =0;
-	public NewClient(JTextField s1, JTextField s2, JTextField s3, /*JTextField s4, */
-			JTextField s5, JTextField s6, JTextField s7) {
+	public NewClient(JTextField s1, JTextField s2, JTextField s3, JTextField s5, JTextField s6, JTextField s7) {
 		this.nom = s1;
 		this.prenom = s2;
 		this.adresse = s3;
-		//this.dateN = s4;
 		this.tel = s5;
 		this.mail = s6;
 		this.sexe = s7;
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		c= new Client(nom.getText(), prenom.getText(), adresse.getText(), tel.getText(), mail.getText(), sexe.getText(), id++);
-		
+		c = new Client(nom.getText(), prenom.getText(), adresse.getText(), tel.getText(), mail.getText(), sexe.getText());
 	}
 }
