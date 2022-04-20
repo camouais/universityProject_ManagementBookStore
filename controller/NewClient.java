17a81cee356171e193fec394d33ae75f1856fdfa
CamilleBert <60038113+CamilleBert@users.noreply.github.com>
2022-04-20 19:07:17 +0200

    Update NewClient.java

--- controller/NewClient.java
+++ controller/NewClient.java
@@ -9,25 +9,28 @@ import view.*;
 
 public class NewClient implements ActionListener{
 	Client c;
-	String nom;
-	String prenom;
-	String adresse;
-	Date dateN;
-	String tel;
-	String mail;
-	String sexe;
-	public NewClient(String s1, String s2, String s3, Date s4, 
-			String s5, String s6, String s7) {
-		nom = s1;
-		prenom = s2;
-		adresse = s3;
-		dateN = s4;
-		tel = s5;
-		mail = s6;
-		sexe = s7;
-	}
+	JTextField nom;
+	JTextField prenom;
+	JTextField adresse;
+	JTextField dateN;
+	JTextField tel;
+	JTextField mail;
+	JTextField sexe;
 	
+	public static int id =0;
+	public NewClient(JTextField s1, JTextField s2, JTextField s3, /*JTextField s4, */
+			JTextField s5, JTextField s6, JTextField s7) {
+		this.nom = s1;
+		this.prenom = s2;
+		this.adresse = s3;
+		//this.dateN = s4;
+		this.tel = s5;
+		this.mail = s6;
+		this.sexe = s7;
+	}
+	@Override
 	public void actionPerformed(ActionEvent e) {
+		c= new Client(nom.getText(), prenom.getText(), adresse.getText(), tel.getText(), mail.getText(), sexe.getText(), id++);
 		
 	}
 }
