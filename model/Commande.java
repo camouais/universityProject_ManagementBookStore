bfbcfaabe04402a461d5e7a2f80920291e1fffae
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-21 21:16:21 +0200

    Update Commande.java

--- model/Commande.java
+++ model/Commande.java
@@ -7,20 +7,43 @@ public class Commande  {
 	// Attributs
 	
 	public static int totalCom;
-	public Date dateAchat;
-	public String modePaiement;
-	public Magasin mag;
-	public int idCom;
-	public Client client;
+	private int idCom;
+	private Date dateAchat;
+	private String modePaiement;
+	private Client client;
 	public Set<Livre> listLivres;
 	
 	// Constructeur
 	
-	public Commande(Date dA, Client c, String mdp, int id) {
+	public Commande(Date dA, Client c, String mdp) {
 		dateAchat= dA;
 		modePaiement = mdp;
 		client=c;
-		idCom =id;
+		idCom = totalCom;
 		totalCom++;
 	}
+    
+    public Date getDateAchat() {
+    	return dateAchat;
+    }
+    
+    public void setNom(Date a) {
+    	dateAchat = a;
+    }
+    
+    public String getModePaiement() {
+    	return modePaiement;
+    }
+    
+    public void setModePaiement(String a) {
+    	modePaiement = a;
+    }
+    
+    public String getClient() {
+    	return modePaiement;
+    }
+    
+    public void setClient(Client a) {
+    	client = a;
+    }
 }
