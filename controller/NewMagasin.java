04492bbacec80a86274b2d183d58c614b4fb504c
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-21 14:20:29 +0200

    Update NewMagasin.java

--- controller/NewMagasin.java
+++ controller/NewMagasin.java
@@ -6,16 +6,20 @@ import java.util.*;
 import model.*;
 import view.*;
 
-public class NewMagasin implements ActionListener {
-    Magasin magasin;
+public class NewMagasin {
+    public Magasin magasin;
     JTextField nom;
     
     public NewMagasin(JTextField n) {
-        this.nom = n;
-    }
-    
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        magasin= new Magasin(nom.getText());
+    	if(n.getText().equals("")) {
+    		System.out.println("name is empty"); //debug
+    		Fen0 a = new Fen0();
+    		a.setVisible(true); // Passer par une variable est visiblement nécessaire, new Fen0() n'est pas suffisant
+    	} else {
+	        this.nom = n;
+	        System.out.println(n.getText());
+	        magasin = new Magasin("name is " + nom.getText()); //debug
+	        new Fen1(magasin);
+    	}
     }
 }
