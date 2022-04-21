164d30a35c44c930321e2f0fc2c85d03541993a6
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-21 21:16:36 +0200

    Update Livraison.java

--- model/Livraison.java
+++ model/Livraison.java
@@ -6,43 +6,40 @@ public class Livraison extends Commande  {
 	
 	// Attributs
 	
-    public String livra_etat;
-    public Magasin magasin;
+    public String etat;
     public Employe livreur;
     public Date dateLiv;
-    public float prixLiv=0;
+    public float prixLiv = 0;
     
     // Constructeurs
     
-    public Livraison(Date dA, Client c, int id,String mdp, Magasin m, Employe l, Date dl) {
-    	super(dA,c,mdp,id);
-    	magasin = m;
+    public Livraison(Date dA, Client c, String mdp, Magasin m, Employe l, Date dl) {
+    	super(dA,c,mdp);
     	livreur = l;
     	dateLiv = dl;
 	}
     
-	public int updateDateLiv(Date d) {
-		if(!d.equals(dateLiv)) {
-			dateLiv = d;
-    		return 0;
-    	}
-    	return -1;
+    public String getEtat() {
+    	return etat;
     }
     
-    public int updateEtat(String e) {
-    	if(!e.equals(livra_etat)) {
-    		livra_etat = e;
-    		return 0;
-    	}
-    	return -1;
+    public void setEtat(String a) {
+    	etat = a;
     }
     
+    public Date getDateLiv() {
+    	return dateLiv;
+    }
     
-    public int editLivreur(Employe l) {
-    	if(!l.equals(livreur)) {
-    		livreur = l;
-    		return 0;
-    	}
-    	return -1;
+    public void setDateLiv(Date a) {
+    	dateLiv = a;
+    }
+    
+    public float getPrixLiv() {
+    	return prixLiv;
+    }
+    
+    public void setPrixLiv(int a) {
+    	prixLiv = a;
     }
 }
