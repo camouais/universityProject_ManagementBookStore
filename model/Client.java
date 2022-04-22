d0d4deabbec6077972c811390cad38c9e94f6e1e
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-22 16:31:23 +0200

    Update Client.java

--- model/Client.java
+++ model/Client.java
@@ -21,11 +21,11 @@ public class Client {
     
     // Constructeur
 
-    public Client(String n, String p, String ad, String m, String t, String am, String s) {
+    public Client(String n, String p, String ad, String t, String am, String s) {
     	nom = n;
     	prenom = p;
     	sexe = s;
-    	mail = m;
+    	mail = am;
     	tel = t;
     	dateCreationCompte = new Date(System.currentTimeMillis());
     	adresse = ad;
