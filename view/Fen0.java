712ec63b531cb43a65f489d32541a575b0ee578b
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-22 15:29:03 +0200

    Update Fen0.java

--- view/Fen0.java
+++ view/Fen0.java
@@ -19,6 +19,7 @@ public class Fen0 extends JFrame {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(240, 230, 140));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
