805e7093ef7f78e9b82bb83d7770a9b60d1d27c6
CamilleBert <60038113+CamilleBert@users.noreply.github.com>
2022-04-24 23:14:41 +0200

    Update Modif_InfoClient.java

--- controller/Modif_InfoClient.java
+++ controller/Modif_InfoClient.java
@@ -1,5 +1,17 @@
 package controller;
 
-public class Modif_InfoClient {
+import java.awt.event.*;
+import javax.swing.*;
+import model.*;
 
+public class Modif_InfoClient {
+	public Modif_InfoClient
+		(Magasin m, Client e, JTextField nom, JTextField prenom,  
+				JTextField adresse, JTextField mail, JTextField tel) {
+			 e.setNom(nom.getText());
+			 e.setPrenom(prenom.getText());
+			 e.setAdresse(adresse.getText());
+			 e.setMail(mail.getText());
+			 e.setTel(tel.getText());
+	}
 }
