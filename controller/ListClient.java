1d68cbae2ffef5c9d23c55204e3a4c6dbdd94a4a
CamilleBert <60038113+CamilleBert@users.noreply.github.com>
2022-04-24 23:15:57 +0200

    Create ListClient.java

---
+++ controller/ListClient.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.Client;
+import model.Magasin;
+
+public class ListClient {
+	Object[] list;
+	String[] formattedList;
+	
+	public ListClient(Magasin m) {
+		list = m.listClient.toArray();
+		formattedList = new String[list.length];
+		System.out.println("Beginning list construction");
+		for(int i = 0; i < list.length; i++) {
+			String b = (((Client)list[i]).getId()+" - " + ((Client)list[i]).getNom() + "  "+ ((Client)list[i]).getPrenom());
+			formattedList[i] = b;
+			System.out.println(b);
+		}
+		System.out.println("Ending list construction");
+	}
+	
+	public String[] getListCli() {
+		return formattedList;
+	}
+}
