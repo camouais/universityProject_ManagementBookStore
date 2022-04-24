8e2b86664282c28db9884302717c863ce1b758cd
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-24 10:47:08 +0200

    Update NewEmploye.java

--- controller/NewEmploye.java
+++ controller/NewEmploye.java
@@ -1,33 +1,30 @@
-
 package controller;
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 import model.*;
 import view.*;
 
 public class NewEmploye {
-	
+
 	Magasin m;
 	Employe e;
-	String nom;
-    String prenom;
-    float salaire;
-    String fonction;
-    String adresse;
-    String mail;
-    String tel;
-    String sexe;
-    public Set<Livraison> listLiv; 
 	
+    public Set<Livraison> listLiv; 
+
 	public NewEmploye(Magasin m, JTextField nom, JTextField prenom, JTextField salaire, JTextField fonction, 
 			JTextField adresse, JTextField mail, JTextField tel) {
+		float tempSalaire = 0;
+		try {
+		      tempSalaire = Float.parseFloat(salaire.getText());
+		} catch (Exception e) {
+			JFrame a = new JFrame();
+		    JOptionPane.showMessageDialog(a, "Valeur invalide dans Salaire", "Erreur", 2);
+		}
 		
-		e = new Employe(nom.getText(), prenom.getText(), Float.parseFloat(salaire.getText()), 
-				fonction.getText(), adresse.getText(), mail.getText(), tel.getText());
+		e = new Employe(nom.getText(), prenom.getText(), tempSalaire, fonction.getText(), adresse.getText(), mail.getText(), tel.getText());
 		m.addEmp(e);
+		System.out.println("Employé créé");
 		new Fen4_Gest_MagEmp(m);
 	}
 }
