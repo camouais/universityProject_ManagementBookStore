1acfb5db8068fc9d1ac0f437f5fea39d2ab11c91
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-24 11:36:35 +0200

    Update ClientExists.java

--- controller/ClientExists.java
+++ controller/ClientExists.java
@@ -6,13 +6,17 @@ import view.*;
 
 public class ClientExists {
 	
+	public Magasin m;
+	public int status;
+	
 	public ClientExists(Magasin m, JTextField n, JTextField p, JTextField mail) {
 		Client a = m.rchCli(n.getText(), p.getText(), mail.getText());
 		if(a == null) {
-			System.out.println("Le client n'existe pas dans le Set<Client> du magasin");
 			JFrame b = new JFrame();
 			JOptionPane.showMessageDialog( b, "Le client n'existe pas dans la liste de clients.", "Erreur", 2);
-			new Fen3_Ach_ExistC(m);
+		} else {
+			status = 1;
+			new Fen4_Ach_DoAch(m,a);
 		}
 	}
 }
