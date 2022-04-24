d053d02dee795c923cb6010a6c1d8d72cf738f58
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-24 11:09:36 +0200

    Update NewClient.java

--- controller/NewClient.java
+++ controller/NewClient.java
@@ -8,6 +8,7 @@ public class NewClient {
 	
 	Magasin m;
 	Client c;
+	public int status;
 	JTextField nom;
 	JTextField prenom;
 	JTextField adresse;
@@ -17,15 +18,24 @@ public class NewClient {
 	JTextField sexe;
 	
 	public NewClient(Magasin m, JTextField nom, JTextField prenom, JTextField adresse, JTextField tel, JTextField mail, JComboBox<String> sexe) {
-		c = new Client(nom.getText(), prenom.getText(), adresse.getText(), tel.getText(), mail.getText(), (String) sexe.getSelectedItem());
-		System.out.println("Client créé avec les attributs suivants :");
-		System.out.println("Nom = " + c.getNom());
-		System.out.println("Prénom = " + c.getPrenom());
-		System.out.println("Adresse = " + c.getAdresse());
-		System.out.println("Téléphone = " + c.getTel());
-		System.out.println("Mail = " + c.getMail());
-		System.out.println("Sexe = " + c.getSexe());
-		m.addCli(c);
-		new Fen4_Ach_DoAch(m,c);
+		if (nom.getText().isEmpty()) {
+			JFrame a = new JFrame();
+		    JOptionPane.showMessageDialog(a, "Nom vide. Veuillez saisir un nom.", "Erreur", 2);
+		} else if (prenom.getText().isEmpty()) {
+			JFrame a = new JFrame();
+		    JOptionPane.showMessageDialog(a, "Prénom vide. Veuillez saisir un prénom.", "Erreur", 2);
+		} else {
+			c = new Client(nom.getText(), prenom.getText(), adresse.getText(), tel.getText(), mail.getText(), (String) sexe.getSelectedItem());
+			System.out.println("Client créé avec les attributs suivants :");
+			System.out.println("Nom = " + c.getNom());
+			System.out.println("Prénom = " + c.getPrenom());
+			System.out.println("Adresse = " + c.getAdresse());
+			System.out.println("Téléphone = " + c.getTel());
+			System.out.println("Mail = " + c.getMail());
+			System.out.println("Sexe = " + c.getSexe());
+			m.addCli(c);
+			new Fen4_Ach_DoAch(m,c);
+			status = 1;
+		}
 	}
 }
