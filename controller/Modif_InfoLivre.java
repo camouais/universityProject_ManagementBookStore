867a7fd92cdde88bf9ffdf8647a94189e54dca44
CamilleBert <60038113+CamilleBert@users.noreply.github.com>
2022-04-25 21:21:31 +0200

    Update Modif_InfoLivre.java

--- controller/Modif_InfoLivre.java
+++ controller/Modif_InfoLivre.java
@@ -1,5 +1,18 @@
 package controller;
 
-public class Modif_InfoLivre {
+import java.awt.event.*;
+import javax.swing.*;
 
+import model.*;
+import view.*;
+
+public class Modif_InfoLivre {
+    public Modif_InfoLivre
+    (Magasin m, Livre l, JTextField titre,JTextField auteur, JTextField editeur,JTextField prix, JTextField categorie) {
+    	l.setTitre(titre.getText());
+		l.setAuteur(auteur.getText());
+		l.setEditeur(editeur.getText());
+		l.setPrix(Float.parseFloat(prix.getText()));
+		l.setCategorie(categorie.getText());
+    }
 }
