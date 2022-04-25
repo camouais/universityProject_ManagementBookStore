ef04345c43116a9d1689fe0d9fd04bf10e987e1c
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-25 23:16:45 +0200

    Update NewMagasin.java

--- controller/NewMagasin.java
+++ controller/NewMagasin.java
@@ -1,6 +1,5 @@
 package controller;
 
-import java.util.Vector;
 import java.util.*;
 import javax.swing.*;
 import model.*;
@@ -16,47 +15,44 @@ public class NewMagasin {
     	Employe e_3 = new Employe("Van", "Jean", 3214, "Vendeur", "11 Rue de la Tour Eiffel", "Van.Jean@gmail.com", "063367195");
     	Employe e_4 = new Employe("Djisk", "Lili", 5425, "Vendeur", "45 Avenue des Chevaleries", "Djisk.Lili@gmail.com", "09364136");
     	Employe e_5 = new Employe("Wo", "Serena", 10001, "Responsable", "78 Rue des Savants", "Wo.Serena@gmail.com", "06546775");
-		
-    	
     	
     	Livre liv1 = new Livre("Le Petit Prince", "Antoine de Saint-Exupéry", "Folio Junior", null, "Littérature pour enfants", 4.87f);
 		Livre liv2 = new Livre("Paroles", "Jacques Prévert", "Folio", null, "Calligraphe", 3.14F);
 		Livre liv3 = new Livre("Candide", "Voltaire", "Poche", null, "Classiques & Patrimoine", 2.85f);
 		Livre liv4 = new Livre("Antigone", "Jean Anouilh", "Poche", null, "Pièce de théâtre", 6.10F);
-		Livre liv5= new Livre("La Promesse de l'aube", "Romain Gary", "Folio Junior", null, "Autobiographie", 8.70f);
+		Livre liv5 = new Livre("La Promesse de l'aube", "Romain Gary", "Folio Junior", null, "Autobiographie", 8.70f);
 		
 		Client cl1 = new Client("Dia", "Cameron", "30 Avenue du Parc", "0676412933", "Cameron.Dia@gmail.com", "Homme" );
 		Client cl2 = new Client("Ba", "Margot", "55 Avenue des Champs-Élysées", "0605486270", "BaMargot@hotmail.fr", "Femme");
 		Client cl3 = new Client("Philipe", "Victor", "87 Rue Victor Hugo", "0627266238", "phiVi@gmail.com", "Homme");
 		Client cl4 = new Client("Anderson", "Jean", "1 Rue Saint-Rustique", "0608539386", "Anderson.Jean@hotmail.fr", "Homme");
-		Client cl5= new Client("Du Château", "Romain", "22 Rue Oberkampf", "0698255138", "Romain.DuCH@gmail.com", "Homme");
-		
+		Client cl5 = new Client("Du Château", "Romain", "22 Rue Oberkampf", "0698255138", "Romain.DuCH@gmail.com", "Homme");
+
 		Livre[] arr1 = { liv1, liv2 };
-		Vector<Livre> v1 = new Vector<Livre>();
-		 for (int i = 0; i < arr1.length; i++)
-	            v1.addElement(arr1[i]);
-    
+		Vector<Livre> v1 = new Vector<Livre>(Arrays.asList(arr1));
 		Commande com1 = new Commande("12-01-2022",cl1,v1); 
-		
-		Livre[] arr2 = { liv1, liv2,liv3 };
+
+		Livre[] arr2 = { liv1, liv2, liv3 };
 		Vector<Livre> v2 = new Vector<Livre>(Arrays.asList(arr2));
 		Commande com2 = new Commande("12-01-2022",cl2,v2); 
-		
 		
 		Livre[] arr3 = { liv1 };
 		Vector<Livre> v3 = new Vector<Livre>(Arrays.asList(arr3));
 		Commande com3 = new Commande("12-01-2022",cl3,v3); 
 		
-		Livre[] arr4 = { liv2, liv4};
+		Livre[] arr4 = { liv2, liv4 };
 		Vector<Livre> v4 = new Vector<Livre>(Arrays.asList(arr4));
 		Commande com4 = new Commande("12-01-2022",cl4,v4); 
 		
-		Livre[] arr5 = { liv1, liv2 ,liv4, liv5};
+		Livre[] arr5 = { liv1, liv2, liv4, liv5 };
 		Vector<Livre> v5 = new Vector<Livre>(Arrays.asList(arr5));
 		Commande com5 = new Commande("12-01-2022",cl5,v5); 
 		
     	if(n.getText().equals("")) {
+    		new Fen0();
     	} else {
+    		
+	        System.out.println("Magasin créé avec le nom : " + n.getText());
 	        Magasin m = new Magasin(n.getText());
 	        m.addEmp(e_0);
 	        m.addEmp(e_1);
@@ -64,13 +60,13 @@ public class NewMagasin {
 	        m.addEmp(e_3);
 	        m.addEmp(e_4);
 	        m.addEmp(e_5);
-	        
+
 			m.addLivre(liv1);
 			m.addLivre(liv2);
 			m.addLivre(liv3);
 			m.addLivre(liv4);
 			m.addLivre(liv5);
-			
+
 			m.addCli(cl1);
 			m.addCli(cl2);
 			m.addCli(cl3);
@@ -82,6 +78,7 @@ public class NewMagasin {
 			m.addCom(com3);
 			m.addCom(com4);
 			m.addCom(com5);
+			
 	        new Fen1(m);
     	}
     }
