18c385833aceca5ad99850dd31fe165d497c997f
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-25 17:22:30 +0200

    Update ListLivres.java

--- controller/ListLivres.java
+++ controller/ListLivres.java
@@ -12,14 +12,10 @@ public class ListLivres {
 		if(livres != null) {
 			list = livres.toArray();
 			formattedList = new String[list.length];
-			System.out.println("Beginning list construction");
 			for(int i = 0; i < list.length; i++) {
 				String b = (((Livre) list[i]).id + " - " + ((Livre) list[i]).titre);
 				formattedList[i] = b;
-				System.out.println(b);
 			}
-			System.out.println("Ending list construction");
-		} else {
 		}
 	}
 	
