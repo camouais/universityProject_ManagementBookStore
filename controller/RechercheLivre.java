f673042d53108cab30195501653276f6b0b4b766
spipanor <68026807+spipanor@users.noreply.github.com>
2022-04-25 17:21:48 +0200

    Update RechercheLivre.java

--- controller/RechercheLivre.java
+++ controller/RechercheLivre.java
@@ -13,13 +13,12 @@ public class RechercheLivre {
 		String rechterm = arg.getText();
 		if(rechterm.equals("")) {
 			JFrame frame = new JFrame();
-		    JOptionPane.showMessageDialog(frame, "Recherche vide. Veuillez écrire un terme de recherche.", "Erreur", 2);
+			JOptionPane.showMessageDialog(frame, "Recherche vide. Veuillez écrire un terme de recherche.", "Erreur", 2);
 		} else {
 			for(int i = 0; i < m.listLivre.size(); i++) {
 				String titre = ((Livre) m.listLivre.toArray()[i]).getTitre();
 				int minlength = Math.min(titre.length(), rechterm.length());
 				if(rechterm.toLowerCase().equals(titre.substring(0,minlength).toLowerCase())) {
-					System.out.println(((Livre) m.listLivre.toArray()[i]).getTitre() + " correspond à " + rechterm);
 					a.add((Livre) m.listLivre.toArray()[i]);
 				}
 			}
@@ -27,9 +26,6 @@ public class RechercheLivre {
 	}
 	
 	public Set<Livre> getList() {
-		for(int i = 0; i < a.size(); i++) {
-			System.out.println(a.toArray()[i].toString() + " ajouté dans a");
-		}
 		return a;
 	}
 }
