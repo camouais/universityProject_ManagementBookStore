9a92c03060984cc8b28b008e670d4ee63f31b15d
CamilleBert <60038113+CamilleBert@users.noreply.github.com>
2022-05-01 17:32:05 +0200

    Update NewMagasin.java

--- controller/NewMagasin.java
+++ controller/NewMagasin.java
@@ -12,24 +12,24 @@ public class NewMagasin {
 	
 	public NewMagasin(JTextField n, JPasswordField mdp) {
     	
-		Employe e_0 = new Employe("Alfred", "Jo", 4521, "Caissier", "5 Rue des Sangliers", "Alfred.Jo@gmail.com", "06352192");
-		Employe e_1 = new Employe("Tulipe", "William", 1234, "Responsable", "23 Rue des Sangliers", "Tulipe.William@gmail.com", "09318995");
-		Employe e_2 = new Employe("Armand", "Sarah", 2687, "Caissier", "6 Rue des Sangliers", "Armand.Sarah@gmail.com", "06357196");
-		Employe e_3 = new Employe("Van", "Jean", 3214, "Vendeur", "11 Rue de la Tour Eiffel", "Van.Jean@gmail.com", "063367195");
-		Employe e_4 = new Employe("Djisk", "Lili", 5425, "Vendeur", "45 Avenue des Chevaleries", "Djisk.Lili@gmail.com", "09364136");
-		Employe e_5 = new Employe("Wo", "Serena", 10001, "Responsable", "78 Rue des Savants", "Wo.Serena@gmail.com", "06546775");
+		Employe e_0 = new Employe("Alfred", "Jo", "Homme", 4521.F, "Caissier", "5 Rue des Sangliers", "Alfred.Jo@gmail.com", "06352192");
+		Employe e_1 = new Employe("Tulipe", "William", "Homme", 1234.F, "Responsable", "23 Rue des Sangliers", "Tulipe.William@gmail.com", "09318995");
+		Employe e_2 = new Employe("Armand", "Sarah", "Femme", 2687.F, "Caissier", "6 Rue des Sangliers", "Armand.Sarah@gmail.com", "06357196");
+		Employe e_3 = new Employe("Van", "Jean", "Homme", 3214.F, "Vendeur", "11 Rue de la Tour Eiffel", "Van.Jean@gmail.com", "063367195");
+		Employe e_4 = new Employe("Djisk", "Lili", "Femme", 5425.F, "Vendeur", "45 Avenue des Chevaleries", "Djisk.Lili@gmail.com", "09364136");
+		Employe e_5 = new Employe("Wo", "Serena", "Femme", 10001.F, "Responsable", "78 Rue des Savants", "Wo.Serena@gmail.com", "06546775");
 		
-		Livre liv1 = new Livre("Le Petit Prince", "Antoine de Saint-ExupÈry", "Folio Junior", null, "LittÈrature pour enfants", 4.87f, 16);
-		Livre liv2 = new Livre("Paroles", "Jacques PrÈvert", "Folio", null, "Calligraphe", 3.14f, 32);
+		Livre liv1 = new Livre("Le Petit Prince", "Antoine de Saint-Exup√©ry", "Folio Junior", null, "Litt√©rature pour enfants", 4.87f, 16);
+		Livre liv2 = new Livre("Paroles", "Jacques Pr√©vert", "Folio", null, "Calligraphe", 3.14f, 32);
 		Livre liv3 = new Livre("Candide", "Voltaire", "Poche", null, "Classiques & Patrimoine", 2.85f, 24);
-		Livre liv4 = new Livre("Antigone", "Jean Anouilh", "Poche", null, "PiËce de thÈ‚tre", 6.10f, 21);
+		Livre liv4 = new Livre("Antigone", "Jean Anouilh", "Poche", null, "Pi√®ce de th√©√¢tre", 6.10f, 21);
 		Livre liv5 = new Livre("La Promesse de l'aube", "Romain Gary", "Folio Junior", null, "Autobiographie", 8.70f, 7);
 		
 		Client cl1 = new Client("Dia", "Cameron", "30 Avenue du Parc", "0676412933", "Cameron.Dia@gmail.com", "Homme", LocalDate.of(1995, 07, 18));
-		Client cl2 = new Client("Ba", "Margot", "55 Avenue des Champs-…lysÈes", "0605486270", "BaMargot@hotmail.fr", "Femme", LocalDate.of(1995, 07, 18));
+		Client cl2 = new Client("Ba", "Margot", "55 Avenue des Champs-√âlys√©es", "0605486270", "BaMargot@hotmail.fr", "Femme", LocalDate.of(1995, 07, 18));
 		Client cl3 = new Client("Philipe", "Victor", "87 Rue Victor Hugo", "0627266238", "phiVi@gmail.com", "Homme", LocalDate.of(1995, 07, 18));
 		Client cl4 = new Client("Anderson", "Jean", "1 Rue Saint-Rustique", "0608539386", "Anderson.Jean@hotmail.fr", "Homme", LocalDate.of(1995, 07, 18));
-		Client cl5 = new Client("Du Ch‚teau", "Romain", "22 Rue Oberkampf", "0698255138", "Romain.DuCH@gmail.com", "Homme", LocalDate.of(1995, 07, 18));
+		Client cl5 = new Client("Du Ch√¢teau", "Romain", "22 Rue Oberkampf", "0698255138", "Romain.DuCH@gmail.com", "Homme", LocalDate.of(1995, 07, 18));
 		
 		Livre[] arr1 = { liv1, liv2 };
 		Vector<Livre> v1 = new Vector<Livre>(Arrays.asList(arr1));
@@ -58,6 +58,7 @@ public class NewMagasin {
    			JFrame a = new JFrame();
    			JOptionPane.showMessageDialog(a, "Mot de passe vide. Veuillez saisir un mot de passe.", "Erreur", 2);
    		} else {
+   			System.out.println("Magasin cr√©√© avec le nom : " + n.getText() + " et le mot de passe " + new String(mdp.getPassword()));
    			Magasin m = new Magasin(n.getText(), new String(mdp.getPassword()));
    			
    			m.addEmp(e_0);
