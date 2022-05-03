1b8ccda5078c6b6225825cb7045cb8118b7ee8df
spipanor <68026807+spipanor@users.noreply.github.com>
2022-05-03 23:00:12 +0200

    Update ListCommande.java

--- controller/ListCommande.java
+++ controller/ListCommande.java
@@ -1,7 +1,6 @@
 package controller;
 
-import java.util.Set;
-
+import java.util.*;
 import model.*;
 
 public class ListCommande {
@@ -9,21 +8,21 @@ public class ListCommande {
 	String[] formattedList;
 	
 	public ListCommande(Magasin m, Set<Commande> c) {
-		if(c!=null) {
-		list = c.toArray();
-		formattedList = new String[list.length];
-		for(int i = 0; i < list.length; i++) {
-			String b = (((Commande)list[i]).getId()+" - Prix Total : "+ 
-						(((Commande)list[i]).getPrixTotal()+" - Client : " + 
-						((Commande)list[i]).getClient().getId() + "  "+ 
-						((Commande)list[i]).getClient().getNom()+ "  "+ 
-						((Commande)list[i]).getClient().getPrenom()));
-			formattedList[i] = b;
-		}
+		if(c != null) {
+			list = m.listCom.toArray();
+			formattedList = new String[list.length];
+			for(int i = 0; i < list.length; i++) {
+				String b = (((Commande)list[i]).getId()+" - Prix Total : "+ 
+							(((Commande)list[i]).getPrixTotal()+" - Client : " + 
+							((Commande)list[i]).getClient().getId() + " ("+ 
+							((Commande)list[i]).getClient().getNom()+ " "+ 
+							((Commande)list[i]).getClient().getPrenom() + ")"));
+				formattedList[i] = b;
+			}
 		}
 	}
 	
-	public String[] getList() {
+	public String[] getListCom() {
 		return formattedList;
 	}
 }
