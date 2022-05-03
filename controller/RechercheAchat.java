f507bba583ced6c08f899061bbbf6a43e6239f3a
spipanor <68026807+spipanor@users.noreply.github.com>
2022-05-03 23:34:46 +0200

    Update RechercheAchat.java

--- controller/RechercheAchat.java
+++ controller/RechercheAchat.java
@@ -8,53 +8,52 @@ public class RechercheAchat {
 	Set<Commande> a;
 
 	String s;
-	
+
 	public RechercheAchat(Magasin m, JTextField arg, String f) {
 		a = new HashSet<Commande>();
-		
-		if(		
-				f.equals("id achat")||
-				f==("Nom client")|| 
-				f==("Prenom client") || 
-				f.equals("id client") || 
-				f==("Date")
-				) {
-		String rechterm = arg.getText();
-		if(rechterm.equals("")) {
-			JFrame frame = new JFrame();
-			JOptionPane.showMessageDialog(frame, "Recherche vide. Veuillez �crire un terme de recherche.", "Erreur", 2);
-		} else {
-			for(int i = 0; i < m.listCom.size(); i++) {
-				
-			if(f =="Nom client") {
-				s = ((Commande) m.listCom.toArray()[i]).getClient().getNom();
-			}
-			else if(f =="Prenom client") {
-				s = (((Commande) m.listCom.toArray()[i]).getClient().getPrenom());
-			}
-			else if(f =="id client") {
-				s = String.valueOf(((Commande) m.listCom.toArray()[i]).getClient().getId());
-			}
-			else if(f =="id achat") {
-				s = String.valueOf(((Commande) m.listCom.toArray()[i]).getId());
-			}
-		
-			else if(f =="Date") {
-				s = (((Commande) m.listCom.toArray()[i]).getDateAchat()).toString();
-			}	
+
+		if(	f.equals("id achat")||
+			f.equals("Nom client")|| 
+			f.equals("Prenom client") || 
+			f.equals("id client") || 
+			f.equals("Date")) {
 			
-			
-			int minlength = Math.min(s.length(), rechterm.length());
-				if(rechterm.toLowerCase().equals(s.substring(0,minlength).toLowerCase())) {
-					a.add((Commande) m.listCom.toArray()[i]);
-				
+			String rechterm = arg.getText();
+			if(rechterm.equals("")) {
+				JFrame frame = new JFrame();
+				JOptionPane.showMessageDialog(frame, "Recherche vide. Veuillez écrire un terme de recherche.", "Erreur", 2);
+			} else {
+				for(int i = 0; i < m.listCom.size(); i++) {
+	
+				if(f =="Nom client") {
+					s = ((Commande) m.listCom.toArray()[i]).getClient().getNom();
+				}
+				else if(f =="Prenom client") {
+					s = (((Commande) m.listCom.toArray()[i]).getClient().getPrenom());
+				}
+				else if(f =="id client") {
+					s = String.valueOf(((Commande) m.listCom.toArray()[i]).getClient().getId());
+				}
+				else if(f =="id achat") {
+					s = String.valueOf(((Commande) m.listCom.toArray()[i]).getId());
+				}
+	
+				else if(f =="Date") {
+					s = (((Commande) m.listCom.toArray()[i]).getDateAchat()).toString();
+				}	
+	
+	
+				int minlength = Math.min(s.length(), rechterm.length());
+					if(rechterm.toLowerCase().equals(s.substring(0,minlength).toLowerCase())) {
+						a.add((Commande) m.listCom.toArray()[i]);
+	
+					}
 				}
 			}
-		}}
-		else {
+		} else {
 			JFrame frame = new JFrame();
 			JOptionPane.showMessageDialog(frame, "Recherche vide. Veuillez sélectionner un filtre.", "Erreur", 2);
-		
+
 		}
 	}
 	public Set<Commande> getList() {
