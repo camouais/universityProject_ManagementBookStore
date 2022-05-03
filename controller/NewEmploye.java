b6481d0b7364fdf2929182dc899a9bcf50128634
spipanor <68026807+spipanor@users.noreply.github.com>
2022-05-03 23:07:13 +0200

    Update NewEmploye.java

--- controller/NewEmploye.java
+++ controller/NewEmploye.java
@@ -22,25 +22,25 @@ public class NewEmploye {
 		    JOptionPane.showMessageDialog(a, "Nom vide. Veuillez saisir un nom.", "Erreur", 2);
 		} else if (prenom.getText().isEmpty()) {
 			JFrame a = new JFrame();
-		    JOptionPane.showMessageDialog(a, "Pr�nom vide. Veuillez saisir un pr�nom.", "Erreur", 2);
+		    JOptionPane.showMessageDialog(a, "Prénom vide. Veuillez saisir un prénom.", "Erreur", 2);
 		} else if (adresse.getText().isEmpty()) {
 			JFrame a = new JFrame();
 		    JOptionPane.showMessageDialog(a, "Adresse vide. Veuillez saisir une adresse.", "Erreur", 2);
 		} else if (tel.getText().isEmpty()) {
 			JFrame a = new JFrame();
-		    JOptionPane.showMessageDialog(a, "Aucun num�ro de t�l�phone renseign�. Veuillez en renseigner un.", "Erreur", 2);
+		    JOptionPane.showMessageDialog(a, "Aucun numéro de téléphone renseigné. Veuillez en renseigner un.", "Erreur", 2);
 		} else if (mail.getText().isEmpty()) {
 			JFrame a = new JFrame();
-		    JOptionPane.showMessageDialog(a, "Aucune adresse mail renseign�e. Veuillez en renseigner une.", "Erreur", 2);
+		    JOptionPane.showMessageDialog(a, "Aucune adresse mail renseignée. Veuillez en renseigner une.", "Erreur", 2);
 		} else if (salaire.getText().isEmpty()) {
 			JFrame a = new JFrame();
-			JOptionPane.showMessageDialog(a, "Aucun salaire renseign�. Veuillez en renseigner un.", "Erreur", 2);
+			JOptionPane.showMessageDialog(a, "Aucun salaire renseigné. Veuillez en renseigner un.", "Erreur", 2);
 		} else if (fonction.getText().isEmpty()) {
 			JFrame a = new JFrame();
-			JOptionPane.showMessageDialog(a, "Aucune fonction renseign�e. Veuillez en renseigner une.", "Erreur", 2);
+			JOptionPane.showMessageDialog(a, "Aucune fonction renseignée. Veuillez en renseigner une.", "Erreur", 2);
 		} else if (salaire.getText().isEmpty()) {
 			JFrame a = new JFrame();
-			JOptionPane.showMessageDialog(a, "Aucun salaire renseign�. Veuillez en renseigner un.", "Erreur", 2);
+			JOptionPane.showMessageDialog(a, "Aucun salaire renseigné. Veuillez en renseigner un.", "Erreur", 2);
 		} else {
 			if(checksum > 0) {
 				try {
@@ -48,12 +48,12 @@ public class NewEmploye {
 					    dateTemp = LocalDate.of(Integer.parseInt(annee.getText()), mois.getSelectedIndex() + 1, jour.getSelectedIndex() + 1);
 					} else {
 						JFrame a = new JFrame();
-						JOptionPane.showMessageDialog(a, "Veuillez saisir une ann�e comprise entre 1910 et 2010.", "Erreur", 2);
+						JOptionPane.showMessageDialog(a, "Veuillez saisir une année comprise entre 1910 et 2010.", "Erreur", 2);
 						checksum = -1;
 					}
 				} catch (Exception e) {
 					JFrame a = new JFrame();
-				    JOptionPane.showMessageDialog(a, "Date invalide. Veuillez r�essayer.", "Erreur", 2);
+				    JOptionPane.showMessageDialog(a, "Date invalide. Veuillez réessayer.", "Erreur", 2);
 				    checksum = -1;
 				}
 			}
@@ -67,7 +67,7 @@ public class NewEmploye {
 				}
 			}
 			if(checksum > 0) {
-				e = new Employe(nom.getText(), prenom.getText(), sexe.getText(), tempSalaire, fonction.getText(), adresse.getText(), mail.getText(), tel.getText(), dateTemp);
+				e = new Employe(nom.getText(), prenom.getText(), tempSalaire, fonction.getText(), adresse.getText(), mail.getText(), tel.getText(), dateTemp);
 				m.addEmp(e);
 				new Fen4_Gest_MagEmp(m);
 				status = 1;
