e92a5d0bdb5f5ec1fe6d5d25532e291bd8f5d8da
spipanor <68026807+spipanor@users.noreply.github.com>
2022-05-03 23:06:00 +0200

    Update NewClient.java

--- controller/NewClient.java
+++ controller/NewClient.java
@@ -8,42 +8,52 @@ import view.*;
 
 public class NewClient {
 	
-	
 	Client c;
 	public int status;
 	
-	
 	public NewClient(Magasin m, JTextField nom, JTextField prenom, JTextField adresse, JTextField tel, JTextField mail, JComboBox<String> sexe, JComboBox jour, JComboBox mois, JTextField annee) {
+		int checksum = 1;
+		LocalDate dateTemp = null;
 		if (nom.getText().isEmpty()) {
 			JFrame a = new JFrame();
 		    JOptionPane.showMessageDialog(a, "Nom vide. Veuillez saisir un nom.", "Erreur", 2);
 		} else if (prenom.getText().isEmpty()) {
 			JFrame a = new JFrame();
-		    JOptionPane.showMessageDialog(a, "Pr�nom vide. Veuillez saisir un pr�nom.", "Erreur", 2);
+		    JOptionPane.showMessageDialog(a, "Prénom vide. Veuillez saisir un prénom.", "Erreur", 2);
 		} else if (adresse.getText().isEmpty()) {
 			JFrame a = new JFrame();
 		    JOptionPane.showMessageDialog(a, "Adresse vide. Veuillez saisir une adresse.", "Erreur", 2);
 		} else if (tel.getText().isEmpty()) {
 			JFrame a = new JFrame();
-		    JOptionPane.showMessageDialog(a, "Aucun num�ro de t�l�phone renseign�. Veuillez en renseigner un.", "Erreur", 2);
+		    JOptionPane.showMessageDialog(a, "Aucun numéro de téléphone renseigné. Veuillez en renseigner un.", "Erreur", 2);
 		} else if (mail.getText().isEmpty()) {
 			JFrame a = new JFrame();
-		    JOptionPane.showMessageDialog(a, "Aucune adresse mail renseign�e. Veuillez en renseigner une.", "Erreur", 2);
+		    JOptionPane.showMessageDialog(a, "Aucune adresse mail renseignée. Veuillez en renseigner une.", "Erreur", 2);
 		} else if (annee.getText().isEmpty()) {
 			JFrame a = new JFrame();
-			JOptionPane.showMessageDialog(a, "Aucune ann�e de naissance renseign�e. Veuillez en s�lectionner une.", "Erreur", 2);
+			JOptionPane.showMessageDialog(a, "Aucune année de naissance renseignée. Veuillez en sélectionner une.", "Erreur", 2);
 		} else {
-			try {
-			      int p = Integer.parseInt(annee.getText());
-			      LocalDate dateTemp = LocalDate.of(Integer.parseInt(annee.getText()), Integer.parseInt(String.valueOf(mois.getSelectedIndex() + 1)), Integer.parseInt((String) jour.getSelectedItem()));
-			      c = new Client(nom.getText(), prenom.getText(), adresse.getText(), tel.getText(), mail.getText(), (String) sexe.getSelectedItem(), dateTemp);
-			      m.addCli(c);
-			      new Fen4_Ach_DoAch(m,c);
-			      status = 1;
-			} catch (Exception e) {
-				JFrame a = new JFrame();
-			    JOptionPane.showMessageDialog(a, "Valeur invalide dans Ann�e. Veuillez r�essayer.", "Erreur", 2);
+			if(checksum > 0) {
+				try {
+					if(1910 <= Integer.parseInt(annee.getText()) && Integer.parseInt(annee.getText()) <= 2010) {
+					    dateTemp = LocalDate.of(Integer.parseInt(annee.getText()), mois.getSelectedIndex() + 1, jour.getSelectedIndex() + 1);
+					} else {
+						JFrame a = new JFrame();
+						JOptionPane.showMessageDialog(a, "Veuillez saisir une année comprise entre 1910 et 2010.", "Erreur", 2);
+						checksum = -1;
+					}
+				} catch (Exception e) {
+					JFrame a = new JFrame();
+				    JOptionPane.showMessageDialog(a, "Date invalide. Veuillez réessayer.", "Erreur", 2);
+				    checksum = -1;
+				}
+			}
+			if (checksum > 0) {
+		      c = new Client(nom.getText(), prenom.getText(), adresse.getText(), tel.getText(), mail.getText(), (String) sexe.getSelectedItem(), dateTemp);
+		      m.addCli(c);
+		      new Fen4_Ach_DoAch(m,c);
+		      status = 1;
 			}
 		}
 	}
-}
+} 
